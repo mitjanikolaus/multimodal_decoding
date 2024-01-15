a46bf76c0f860a5a789e103cde9ffe5dcc1bbf49
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-01-15 01:13:23 +0100

    fix for gpu

--- feature_extraction/extract_visualbert_features.py
+++ feature_extraction/extract_visualbert_features.py
@@ -169,10 +169,10 @@ def select_boxes(cfg, output_boxes, scores):
     cls_boxes = output_boxes.tensor.detach().reshape(1000, 80, 4)
     max_conf = torch.zeros((cls_boxes.shape[0]))
     for cls_ind in range(0, cls_prob.shape[1] - 1):
-        cls_scores = cls_prob[:, cls_ind + 1].detach().cpu()
+        cls_scores = cls_prob[:, cls_ind + 1]
         det_boxes = cls_boxes[:, cls_ind, :]
         keep = nms(det_boxes, cls_scores, test_nms_thresh).cpu().numpy()
-        max_conf[keep] = torch.where(cls_scores[keep] > max_conf[keep], cls_scores[keep], max_conf[keep])
+        max_conf[keep] = torch.where(cls_scores[keep] > max_conf[keep], cls_scores[keep].detach().cpu(), max_conf[keep])
     keep_boxes = torch.where(max_conf >= test_score_thresh)[0]
     return keep_boxes, max_conf
 
