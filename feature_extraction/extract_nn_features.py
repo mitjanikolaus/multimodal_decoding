5eb07d0eaf0f8dd9ec58ed630bafae6d6210d417
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-01-17 18:13:45 +0100

    base nn feature extraction

--- feature_extraction/extract_nn_features.py
+++ feature_extraction/extract_nn_features.py
@@ -1,58 +1,16 @@
-###############################
-# Save features of various dnns
-# The script contains several utility functions to extract latent vectors (or features) from DNNs and apply PCA if
-# necessary. Current functions might not be suitable for all types of models. You can always add your own functions.
-# The important thing is to store the data in a python dictionary with the following nested structure (to be compatible
-# with other functions and the analysis scripts):
-#
-# ```
-# ---------------
-# Language models
-# ---------------
-#
-# dict
-# |
-# └── coco_stimulus_id (integer): dict
-#     |
-#     └── 'lingual_feature': 1-d numpy array (feature vector)
-#     |
-#     └── 'caption': the corresponding caption (string)
-#
-# -------------
-# Vision models
-# -------------
-#
-# dict
-# |
-# └── coco_stimulus_id (integer): dict
-#     |
-#     └── 'visual_feature': 1-d numpy array (feature vector)
-#     |
-#     └── 'name': image file name (string)
-###############################
-
 import os
 import torch
-from transformers import pipeline
+from transformers import pipeline, ViTModel, ViTImageProcessor, AutoFeatureExtractor, ResNetModel, BertModel, \
+    BertTokenizer, GPT2Tokenizer, GPT2Model
 import numpy as np
-from torch.utils.data import DataLoader, Dataset
 from glob import glob
-from os.path import join
 from PIL import Image
 import pickle
 from tqdm import tqdm
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from torchvision.transforms import functional as F, InterpolationMode
-from typing import Tuple
-from torch import nn, Tensor
 
-from feature_extraction.feat_extraction_utils import COCOSelected
-from utils import FEATURES_DIR, IMAGES_IMAGERY_CONDITION, COCO_2017_TRAIN_IMAGES_DIR, CAPTIONS_PATH, \
-    IMAGES_TEST, PCA_NUM_COMPONENTS, SUBJECTS, IMAGERY_SCENES, STIMULI_IDS_PATH, TWO_STAGE_GLM_DATA_DIR, LANG_FEAT_KEY, \
-    VISION_FEAT_KEY
-
-NUM_WORKERS = 8
+from feature_extraction.feat_extraction_utils import FeatureExtractor
+from utils import SUBJECTS, IMAGERY_SCENES, STIMULI_IDS_PATH, TWO_STAGE_GLM_DATA_DIR, LANG_FEAT_KEY, \
+    model_features_file_path
 
 BATCH_SIZE = 512
 
@@ -62,11 +20,6 @@ if torch.cuda.is_available():
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 else:
     device = "cpu"
-
-print("device: ", device)
-
-IMAGENET_MEAN = (0.485, 0.456, 0.406)
-IMAGENET_STD = (0.229, 0.224, 0.225)
 
 
 def load_and_save_relevant_coco_ids():
@@ -95,272 +48,86 @@ def load_and_save_relevant_coco_ids():
         pickle.dump(all_ids, open(STIMULI_IDS_PATH, "wb"))
 
 
-def find_coco_image(sid, coco_images_dir):
-    r"""
-    Helper function to find coco image address based on the id.
-    """
-    file_name = f"{sid:012d}.jpg"
-    image_add = list(glob(join(coco_images_dir, '*', file_name)))
-    return image_add[0]
+class ViTFeatureExtractor(FeatureExtractor):
+
+    def extract_features_from_batch(self, ids, captions, img_paths):
+        images = [Image.open(path) for path in img_paths]
+        images = [img.convert('RGB') if img.mode != 'RGB' else img for img in images]
+
+        inputs = self.preprocessor(images=images, return_tensors="pt")
+        inputs = inputs.to(self.device)
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+
+        last_hidden_state = outputs.last_hidden_state
+        feats_vision = last_hidden_state[:, 0, :]
+        return None, feats_vision
 
 
-def get_visual_features(model, dataloader):
-    r"""
-    Helper function to get feature from Pytorch compatible vision models.
+class ResNetFeatureExtractor(FeatureExtractor):
 
-    Args:
-        `model` (torch.nn): Pytorch model
-        `dataloader` (DataLoader): Pytorch data loader to provide the inputs
+    def extract_features_from_batch(self, ids, captions, img_paths):
+        images = [Image.open(path) for path in img_paths]
+        images = [img.convert('RGB') if img.mode != 'RGB' else img for img in images]
 
-    Returns:
-        `features` (dict): extracted features
-    """
-    features = {}
-    model.to(device)
-    model.eval()
-    temp = None
-    is_vision_transformer = isinstance(model, VisionTransformer)
+        inputs = self.preprocessor(images=images, return_tensors="pt")
+        inputs = inputs.to(self.device)
+        with torch.no_grad():
+            outputs = self.model(**inputs)
 
-    def save_rep(layer, inp, output):
-        nonlocal temp
-        if is_vision_transformer:
-            temp = inp[0].cpu().numpy()
-        else:
-            temp = output.cpu().numpy()
-
-    if is_vision_transformer:
-        module = model.heads
-    else:
-        module = model.avgpool
-    module.register_forward_hook(save_rep)
-
-    with tqdm(dataloader, unit="batch") as tepoch:
-        for images, ids, names in tepoch:
-            with torch.no_grad():
-                model(images.to(device))
-            for vector, sid, n in zip(temp, ids, names):
-                features[int(sid.item())] = {VISION_FEAT_KEY: vector.squeeze()}
-    return features
+        feats_vision = outputs.pooler_output.squeeze()
+        return None, feats_vision
 
 
-def get_lingual_features_transformers(model_name, dataloader):
-    r"""
-    Helper function to get features from transformer language models available in huggingface.
-    The final feature vector will be the average of all tokens.
+class LanguageModelFeatureExtractor(FeatureExtractor):
 
-    Args:
-        `model_name` (str): Name of the language model (based on hugging face repositories)
-        `dataloader` (DataLoader): Pytorch data loader to provide the inputs
+    def extract_features(self):
+        all_feats = dict()
+        model = pipeline('feature-extraction', model=self.model_name, device=self.device)
+        with tqdm(self.dloader, unit="batch") as tepoch:
+            for ids, captions, _ in tepoch:
+                ids = [id.item() for id in ids]
 
-    Returns:
-        `features` (dict): extracted features
-    """
-    features = {}
-    model = pipeline('feature-extraction', model=model_name, device=0 if device == "cuda" else device)
-    with tqdm(dataloader, unit="batch") as tepoch:
-        for captions, ids in tepoch:
-            fdata = model(list(captions))
-            for vector, sid, caption in zip(fdata, ids, captions):
-                v = np.array(vector[0]).mean(axis=0)
-                features[int(sid.item())] = {LANG_FEAT_KEY: v.squeeze()}
-    return features
+                with torch.no_grad():
+                    feats_batch = model(list(captions))
+                for feats, id, caption in zip(feats_batch, ids, captions):
+                    feats_mean = np.array(feats[0]).mean(axis=0)
+                    all_feats[id] = {LANG_FEAT_KEY: feats_mean.squeeze()}
 
-
-def apply_pca(n_components, data_pickle_file):
-    r"""
-    Applies PCA on the given latent vectors. The function saves the following (pickle) files
-    in the same directory as the given pickle file for the latent vectors:
-
-    - PCA module (to be used later to recover original vectors)
-    - Scaler module (that used for data normalization before the PCA)
-    - Compressed vectors (transformed latent vectors)
-
-    Args:
-        `n_components` (int): number of PCs
-        `data_pickle_file` (str): address to the pickle file containing latent vectors
-    """
-    print("Performing PCA..")
-    excluded = set(IMAGES_TEST + [a[0] for a in IMAGES_IMAGERY_CONDITION])
-
-    filename = os.path.basename(data_pickle_file)
-    dirname = os.path.dirname(data_pickle_file)
-    base_filename = filename[:filename.find('.p')]
-
-    with open(data_pickle_file, 'rb') as f:
-        data = pickle.load(f)
-
-    # gathering all the stim ids
-    stim_ids = list(data.keys())
-
-    # finding keys for feature values
-    features_names = [k for k in data[stim_ids[0]].keys() if "feature" in k]
-
-    # normalizing first, then PCA
-    transformed_data = {k: {} for k in stim_ids}
-    for feature in features_names:
-        # creating the combined vector with order of stim_ids
-        vectors = []
-        all_vectors = []
-        for sid in stim_ids:
-            if sid not in excluded:  # no influence of the test and imagery sets
-                vectors.append(data[sid][feature])
-
-            all_vectors.append(data[sid][feature])
-
-        vectors = np.array(vectors)
-        all_vectors = np.array(all_vectors)
-
-        scaler = StandardScaler()
-        normalized = scaler.fit_transform(vectors)
-        normalized_all_vectors = scaler.transform(all_vectors)
-
-        pca = PCA(n_components=n_components, whiten=True)  # whiten=True: divide by singular values
-        pca.fit(normalized)
-        reduced_all_vectors = pca.transform(normalized_all_vectors)
-
-        # storing the reduced vectors (keeping stim_id ordering)
-        for idx, sid in enumerate(stim_ids):
-            transformed_data[sid][feature] = reduced_all_vectors[idx]
-
-        # save PCA and scaler modules for future transformations
-        pca_file = join(dirname, f"{base_filename}_pca_{n_components}_pca_{feature}_module.p")
-        with open(pca_file, 'wb') as handle:
-            pickle.dump(pca, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        scaler_file = join(dirname, f"{base_filename}_pca_{n_components}_scaler_{feature}_module.p")
-        with open(scaler_file, 'wb') as handle:
-            pickle.dump(scaler, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    # filling the rest as before
-    for sid in stim_ids:
-        for k in data[sid]:
-            if k not in transformed_data[sid]:
-                transformed_data[sid][k] = data[sid][k]
-
-    # saving transformed data in the same place with PCA_ncomp postfix
-    new_file = join(dirname, f"{base_filename}_pca_{n_components}.p")
-
-    with open(new_file, 'wb') as handle:
-        pickle.dump(transformed_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-class ImageClassificationPreprocessing(nn.Module):
-    def __init__(
-            self,
-            *,
-            crop_size: int,
-            resize_size: int,
-            mean: Tuple[float, ...] = IMAGENET_MEAN,
-            std: Tuple[float, ...] = IMAGENET_STD,
-            interpolation: InterpolationMode = InterpolationMode.BICUBIC,
-    ) -> None:
-        super().__init__()
-        self.crop_size = [crop_size]
-        self.resize_size = [resize_size]
-        self.mean = list(mean)
-        self.std = list(std)
-        self.interpolation = interpolation
-
-    def forward(self, img: Tensor) -> Tensor:
-        img = F.resize(img, self.resize_size, interpolation=self.interpolation, max_size=None, antialias=True)
-        img = F.center_crop(img, self.crop_size)
-        if not isinstance(img, Tensor):
-            img = F.pil_to_tensor(img)
-        img = F.convert_image_dtype(img, torch.float)
-        img = F.normalize(img, mean=self.mean, std=self.std)
-        return img
-
-    def __repr__(self) -> str:
-        format_string = self.__class__.__name__ + "("
-        format_string += f"\n    crop_size={self.crop_size}"
-        format_string += f"\n    resize_size={self.resize_size}"
-        format_string += f"\n    mean={self.mean}"
-        format_string += f"\n    std={self.std}"
-        format_string += f"\n    interpolation={self.interpolation}"
-        format_string += "\n)"
-        return format_string
-
-    def describe(self) -> str:
-        return (
-            "Accepts ``PIL.Image``, batched ``(B, C, H, W)`` and single ``(C, H, W)`` image ``torch.Tensor`` objects. "
-            f"The images are resized to ``resize_size={self.resize_size}`` using ``interpolation={self.interpolation}``, "
-            f"followed by a central crop of ``crop_size={self.crop_size}``. Finally the values are first rescaled to "
-            f"``[0.0, 1.0]`` and then normalized using ``mean={self.mean}`` and ``std={self.std}``."
-        )
-
-
-def extract_visual_features(model, path_out, resize_size, crop_size, interpolation):
-    if os.path.isfile(path_out):
-        print(f"Skipping feature extraction for {model.__class__.__name__} as output file {path_out} already exists.")
-        return
-    print(f"Extracting features with {model.__class__.__name__}..")
-    preprocessing = ImageClassificationPreprocessing(crop_size=crop_size, resize_size=resize_size,
-                                                     interpolation=interpolation)
-    ds = COCOSelected(COCO_2017_TRAIN_IMAGES_DIR, CAPTIONS_PATH, STIMULI_IDS_PATH, 'image', transform=preprocessing)
-    dloader = DataLoader(ds, shuffle=False, num_workers=0, batch_size=BATCH_SIZE)
-    vf = get_visual_features(model, dloader)
-    os.makedirs(os.path.dirname(path_out), exist_ok=True)
-    pickle.dump(vf, open(path_out, 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
-
-    apply_pca(PCA_NUM_COMPONENTS, path_out)
-
-
-def extract_linguistic_features(model_name, path_out):
-    if os.path.isfile(path_out):
-        print(f"Skipping feature extraction for {model_name} as output file {path_out} already exists.")
-        return
-    print(f"Extracting features with {model.__class__.__name__}..")
-
-    ds = COCOSelected(COCO_2017_TRAIN_IMAGES_DIR, CAPTIONS_PATH, STIMULI_IDS_PATH, 'caption')
-    dloader = DataLoader(ds, shuffle=False, num_workers=NUM_WORKERS, batch_size=BATCH_SIZE)
-    cf = get_lingual_features_transformers(model_name, dloader)
-
-    os.makedirs(os.path.dirname(path_out), exist_ok=True)
-    pickle.dump(cf, open(path_out, 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
-    apply_pca(PCA_NUM_COMPONENTS, path_out)
+        path_out = model_features_file_path(self.model_name)
+        os.makedirs(os.path.dirname(path_out), exist_ok=True)
+        pickle.dump(all_feats, open(path_out, 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == "__main__":
-    from torchvision.models import resnet152, vit_l_16, ViT_L_16_Weights, ResNet152_Weights, VisionTransformer
-
-    os.makedirs(FEATURES_DIR, exist_ok=True)
     load_and_save_relevant_coco_ids()
 
-    ##########
-    # ViT-L-16
-    ##########
-    model = vit_l_16(weights=ViT_L_16_Weights.IMAGENET1K_V1)
-    model_name = "vit_l_16"
-    path_out = f"{FEATURES_DIR}/vit/{model_name}_encoder_selected_coco_crop.p"
-    extract_visual_features(model, path_out, resize_size=242, crop_size=224, interpolation=InterpolationMode.BILINEAR)
+    model_name = 'google/vit-large-patch16-224-in21k'
+    feature_extractor = ViTImageProcessor.from_pretrained(model_name)
+    model = ViTModel.from_pretrained(model_name)
+    extractor = ViTFeatureExtractor(model, feature_extractor, "ViT_L_16_21K", BATCH_SIZE, device)
+    extractor.extract_features()
 
-    ##########
-    # ResNet152
-    ##########
-    model = resnet152(weights=ResNet152_Weights.IMAGENET1K_V1)
-    model_name = "resnet152"
-    path_out = f"{FEATURES_DIR}/resnet/{model_name}_avgpool_selected_coco_crop.p"
-    extract_visual_features(model, path_out, resize_size=256, crop_size=224, interpolation=InterpolationMode.BILINEAR)
+    model_name = 'google/vit-large-patch16-224'
+    feature_extractor = ViTImageProcessor.from_pretrained(model_name)
+    model = ViTModel.from_pretrained(model_name)
+    extractor = ViTFeatureExtractor(model, feature_extractor, "ViT_L_16", BATCH_SIZE, device)
+    extractor.extract_features()
 
-    #########
-    # BERT LARGE
-    #########
+    model_name = 'microsoft/resnet-152'
+    feature_extractor = AutoFeatureExtractor.from_pretrained(model_name)
+    model = ResNetModel.from_pretrained(model_name)
+    extractor = ResNetFeatureExtractor(model, feature_extractor, "Resnet-152", BATCH_SIZE, device)
+    extractor.extract_features()
+
     model_name = 'bert-large-uncased'
-    path_out = f"{FEATURES_DIR}/bert/bert_large_avg_selected_coco.p"
-    extract_linguistic_features(model_name, path_out)
+    tokenizer = BertTokenizer.from_pretrained(model_name)
+    model = BertModel.from_pretrained(model_name)
+    extractor = LanguageModelFeatureExtractor(model, tokenizer, model_name, BATCH_SIZE, device)
+    extractor.extract_features()
 
-    #########
-    # GPT-2xl
-    #########
     model_name = 'gpt2-xl'
-    path_out = f"{FEATURES_DIR}/gpt/gpt2_xl_avg_selected_coco.p"
-    extract_linguistic_features(model_name, path_out)
-
-# ds = COCOSelected(COCO_2017_IMAGES_DIR, IMAGE_STIMULI_IDS_PATH, STIMULI_IDS_PATH, 'caption')
-# data = np.load(IMAGE_STIMULI_IDS_PATH, allow_pickle=True)
-# print(data[0])
-# for subject in IMAGERY_SCENES:
-#     scenes = IMAGERY_SCENES[subject]
-#     for s in scenes:
-#         img_add = find_coco_image(s[1], '/home/milad/datasets/coco2017/images')
-#         tail = img_add[img_add.find('/images/'):-1].replace('/',"\\\\")
-#         print(f"[{s[1]}, '{tail}', '{s[0]}']")
+    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+    model = GPT2Model.from_pretrained(model_name)
+    extractor = LanguageModelFeatureExtractor(model, tokenizer, model_name, BATCH_SIZE, device)
+    extractor.extract_features()
--- feature_extraction/feat_extraction_utils.py
+++ feature_extraction/feat_extraction_utils.py
@@ -3,11 +3,13 @@ import pickle
 
 import numpy as np
 from PIL import Image
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 
 from utils import IMAGES_IMAGERY_CONDITION, COCO_2017_TRAIN_IMAGES_DIR, CAPTIONS_PATH, STIMULI_IDS_PATH, FEATURES_DIR, \
-    LANG_FEAT_KEY, VISION_FEAT_KEY, model_features_file_path
+    LANG_FEAT_KEY, VISION_FEAT_KEY, model_features_file_path, IMAGES_TEST
 
 
 class COCOSelected(Dataset):
@@ -77,13 +79,16 @@ class FeatureExtractor:
         for ids, captions, img_paths in tqdm(self.dloader):
             ids = [id.item() for id in ids]
             language_feats_batch, vision_feats_batch = self.extract_features_from_batch(ids, captions, img_paths)
+            if language_feats_batch is None:
+                language_feats_batch = [None] * len(ids)
+            else:
+                language_feats_batch = language_feats_batch.cpu().numpy()
+            if vision_feats_batch is None:
+                vision_feats_batch = [None] * len(ids)
+            else:
+                vision_feats_batch = vision_feats_batch.cpu().numpy()
             for id, feats_lang, feats_vision, path in zip(ids, language_feats_batch, vision_feats_batch, img_paths):
-                if feats_lang.device != "cpu":
-                    feats_lang = feats_lang.cpu()
-                if feats_vision.device != "cpu":
-                    feats_vision = feats_vision.cpu()
-
-                all_feats[id] = {LANG_FEAT_KEY: feats_lang.numpy(), VISION_FEAT_KEY: feats_vision.numpy()}
+                all_feats[id] = {LANG_FEAT_KEY: feats_lang, VISION_FEAT_KEY: feats_vision}
 
         path_out = model_features_file_path(self.model_name)
         os.makedirs(os.path.dirname(path_out), exist_ok=True)
@@ -91,3 +96,81 @@ class FeatureExtractor:
 
     def extract_features_from_batch(self, ids, captions, img_paths):
         raise NotImplementedError()
+
+
+def apply_pca(n_components, data_pickle_file):
+    r"""
+    Applies PCA on the given latent vectors. The function saves the following (pickle) files
+    in the same directory as the given pickle file for the latent vectors:
+
+    - PCA module (to be used later to recover original vectors)
+    - Scaler module (that used for data normalization before the PCA)
+    - Compressed vectors (transformed latent vectors)
+
+    Args:
+        `n_components` (int): number of PCs
+        `data_pickle_file` (str): address to the pickle file containing latent vectors
+    """
+    print("Performing PCA..")
+    excluded = set(IMAGES_TEST + [a[0] for a in IMAGES_IMAGERY_CONDITION])
+
+    filename = os.path.basename(data_pickle_file)
+    dirname = os.path.dirname(data_pickle_file)
+    base_filename = filename[:filename.find('.p')]
+
+    with open(data_pickle_file, 'rb') as f:
+        data = pickle.load(f)
+
+    # gathering all the stim ids
+    stim_ids = list(data.keys())
+
+    # finding keys for feature values
+    features_names = [k for k in data[stim_ids[0]].keys() if "feature" in k]
+
+    # normalizing first, then PCA
+    transformed_data = {k: {} for k in stim_ids}
+    for feature in features_names:
+        # creating the combined vector with order of stim_ids
+        vectors = []
+        all_vectors = []
+        for sid in stim_ids:
+            if sid not in excluded:  # no influence of the test and imagery sets
+                vectors.append(data[sid][feature])
+
+            all_vectors.append(data[sid][feature])
+
+        vectors = np.array(vectors)
+        all_vectors = np.array(all_vectors)
+
+        scaler = StandardScaler()
+        normalized = scaler.fit_transform(vectors)
+        normalized_all_vectors = scaler.transform(all_vectors)
+
+        pca = PCA(n_components=n_components, whiten=True)  # whiten=True: divide by singular values
+        pca.fit(normalized)
+        reduced_all_vectors = pca.transform(normalized_all_vectors)
+
+        # storing the reduced vectors (keeping stim_id ordering)
+        for idx, sid in enumerate(stim_ids):
+            transformed_data[sid][feature] = reduced_all_vectors[idx]
+
+        # save PCA and scaler modules for future transformations
+        pca_file = os.path.join(dirname, f"{base_filename}_pca_{n_components}_pca_{feature}_module.p")
+        with open(pca_file, 'wb') as handle:
+            pickle.dump(pca, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        scaler_file = os.path.join(dirname, f"{base_filename}_pca_{n_components}_scaler_{feature}_module.p")
+        with open(scaler_file, 'wb') as handle:
+            pickle.dump(scaler, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    # filling the rest as before
+    for sid in stim_ids:
+        for k in data[sid]:
+            if k not in transformed_data[sid]:
+                transformed_data[sid][k] = data[sid][k]
+
+    # saving transformed data in the same place with PCA_ncomp postfix
+    new_file = os.path.join(dirname, f"{base_filename}_pca_{n_components}.p")
+
+    with open(new_file, 'wb') as handle:
+        pickle.dump(transformed_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
