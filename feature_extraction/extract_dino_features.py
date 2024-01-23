ce81e79bdb2d95e5246d311f7cb1d73a40b79822
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-01-23 15:04:07 +0100

    update extract dino feats

--- feature_extraction/extract_dino_features.py
+++ feature_extraction/extract_dino_features.py
@@ -12,10 +12,10 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-BATCH_SIZE = 10
+BATCH_SIZE = 100
 
 
-class DynoFeatureExtractor(FeatureExtractor):
+class DinoFeatureExtractor(FeatureExtractor):
 
     def extract_features_from_batch(self, ids, captions, img_paths):
         images = [Image.open(img_path).convert('RGB') for img_path in img_paths]
@@ -24,27 +24,28 @@ class DynoFeatureExtractor(FeatureExtractor):
         with torch.no_grad():
             outputs = model(**inputs)
 
-        last_hidden_states = outputs[0]
+        last_hidden_state = outputs[0]
 
-        feats_vision = last_hidden_states[:, 0, :]
-        return None, feats_vision
+        feats_vision_cls = last_hidden_state[:, 0, :]
+        feats_vision_mean = last_hidden_state[:, 1:].mean(axis=1)
+        return None, feats_vision_mean, feats_vision_cls
 
 
 if __name__ == "__main__":
     model_name = 'facebook/dinov2-base'
     prepocessor = AutoImageProcessor.from_pretrained(model_name)
     model = AutoModel.from_pretrained(model_name)
-    extractor = DynoFeatureExtractor(model, prepocessor, "dino-base", BATCH_SIZE, device)
+    extractor = DinoFeatureExtractor(model, prepocessor, "dino-base", BATCH_SIZE, device)
     extractor.extract_features()
 
     model_name = 'facebook/dinov2-large'
     prepocessor = AutoImageProcessor.from_pretrained(model_name)
     model = AutoModel.from_pretrained(model_name)
-    extractor = DynoFeatureExtractor(model, prepocessor, "dino-large", BATCH_SIZE, device)
+    extractor = DinoFeatureExtractor(model, prepocessor, "dino-large", BATCH_SIZE, device)
     extractor.extract_features()
 
     model_name = 'facebook/dinov2-giant'
     prepocessor = AutoImageProcessor.from_pretrained(model_name)
     model = AutoModel.from_pretrained(model_name)
-    extractor = DynoFeatureExtractor(model, prepocessor, "dino-giant", BATCH_SIZE, device)
+    extractor = DinoFeatureExtractor(model, prepocessor, "dino-giant", BATCH_SIZE, device)
     extractor.extract_features()
