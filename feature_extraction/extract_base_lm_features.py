d2731148fec8421145e773df74262ee1c245acc5
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-01-24 17:20:27 +0100

    llama2-70b

--- feature_extraction/extract_base_lm_features.py
+++ feature_extraction/extract_base_lm_features.py
@@ -76,23 +76,29 @@ if __name__ == "__main__":
     # extractor = LanguageModelFeatureExtractor(model, tokenizer, "gpt2-small", batch_size=BATCH_SIZE, device=device)
     # extractor.extract_features()
 
-    # model_name = "mistralai/Mistral-7B-v0.1"    # mistralai/Mixtral-8x7B-v0.1
+    # model_name = "mistralai/Mistral-7B-v0.1"
     # tokenizer = AutoTokenizer.from_pretrained(model_name)
     # model = MistralModel.from_pretrained(model_name)
     # extractor = LanguageModelFeatureExtractor(model, tokenizer, "mistral-7b", batch_size=10, device="cpu")
     # extractor.extract_features()
 
-    model_name = "mistralai/Mixtral-8x7B-v0.1"   # mistralai/Mixtral-8x7B-v0.1
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = MixtralModel.from_pretrained(model_name, load_in_4bit=True)
-    extractor = LanguageModelFeatureExtractor(model, tokenizer, "mixtral-8x7B", batch_size=100, device=device)
-    extractor.extract_features()
+    # model_name = "mistralai/Mixtral-8x7B-v0.1"
+    # tokenizer = AutoTokenizer.from_pretrained(model_name)
+    # model = MixtralModel.from_pretrained(model_name, load_in_4bit=True)
+    # extractor = LanguageModelFeatureExtractor(model, tokenizer, "mixtral-8x7B", batch_size=100, device=device)
+    # extractor.extract_features()
 
     # model_weights_path = os.path.join(DATA_DIR, "llama2_7b")
     # tokenizer = LlamaTokenizer.from_pretrained(model_weights_path)
     # model = LlamaModel.from_pretrained(model_weights_path)
     # extractor = LanguageModelFeatureExtractor(model, tokenizer, "llama2-7b", batch_size=10, device="cpu")
     # extractor.extract_features()
+
+    model_weights_path = os.path.join(DATA_DIR, "llama2_70b")
+    tokenizer = LlamaTokenizer.from_pretrained(model_weights_path)
+    model = LlamaModel.from_pretrained(model_weights_path)
+    extractor = LanguageModelFeatureExtractor(model, tokenizer, "llama2-70b", batch_size=10, device="cpu")
+    extractor.extract_features()
 
     # model_name = 'facebook/opt-30b'
     # tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
