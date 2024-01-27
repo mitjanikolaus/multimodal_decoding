68e248653e80c8ae030165774bc2ec9f5afd3733
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-01-27 16:18:10 +0100

    rsa for img-caps

--- analyses/calc_noise_ceilings.py
+++ analyses/calc_noise_ceilings.py
@@ -5,6 +5,8 @@ import numpy as np
 import os
 import pickle
 
+from scipy.stats import spearmanr, pearsonr
+
 from analyses.ridge_regression_decoding import get_fmri_data, create_dissimilarity_matrix, GLM_OUT_DIR, \
     rsa_from_matrices
 
@@ -34,9 +36,21 @@ def load_mean_std(subject, mode="train"):
 
 def run(args):
     matrices = dict()
+    all_rsa_img_caps = []
+
     for subj in SUBJECTS:
-        fmri_test_betas, _, _, _ = get_fmri_data(subj, "test", load_mean_std(subj))
+        fmri_test_betas, stim_ids, stim_types, _ = get_fmri_data(subj, "test", load_mean_std(subj))
         matrices[subj] = create_dissimilarity_matrix(fmri_test_betas, args.matrix_metric)
+
+        betas_captions = fmri_test_betas[stim_types == 'caption']
+        betas_images = fmri_test_betas[stim_types == 'image']
+
+        matrix_captions = create_dissimilarity_matrix(betas_captions)
+        matrix_images = create_dissimilarity_matrix(betas_images)
+        rsa_img_caps = rsa_from_matrices(matrix_images, matrix_captions, args.metric)
+        all_rsa_img_caps.append(rsa_img_caps)
+
+    print(f"RSA imgs-caps: {np.mean(all_rsa_img_caps):.2f} Std: {np.std(all_rsa_img_caps):.2f}")
 
     rsa_scores = dict()
     for subj1, subj2 in itertools.combinations(SUBJECTS, 2):
@@ -44,7 +58,7 @@ def run(args):
         rsa_scores[f"{subj1}_{subj2}"] = rsa
 
     values = list(rsa_scores.values())
-    print(f"Mean RSA: {np.mean(values):.2f} Std: {np.std(values):.2f}")
+    print(f"Between-subject RSA: {np.mean(values):.2f} Std: {np.std(values):.2f}")
 
     results_file = os.path.join(RSA_NOISE_CEILING_DIR, f"{args.metric}_{args.matrix_metric}.p")
     os.makedirs(RSA_NOISE_CEILING_DIR, exist_ok=True)
