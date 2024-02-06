76aea3b743b50878fc3b4e126fc0986d060e68fe
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-02-06 13:44:24 +0100

    verbose 1

--- analyses/searchlight.py
+++ analyses/searchlight.py
@@ -143,7 +143,7 @@ def run(args):
 
                             start = time.time()
                             scores = search_light(X, latents, estimator=model, A=adjacency, cv=cv, n_jobs=args.n_jobs,
-                                                  scoring=pairwise_acc_scorer, verbose=3)
+                                                  scoring=pairwise_acc_scorer, verbose=1)
                             end = time.time()
                             print(f"Preprocessing time: {prepr_time}s")
                             print(f"Searchlight time: {int(end - start)}s")
