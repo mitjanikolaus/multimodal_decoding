515a9383be78b3f71f8ea5a2a9b56558fdf60ecc
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-02-08 19:43:24 +0100

    test9 move lingual

--- analyses/ridge_regression_decoding.py
+++ analyses/ridge_regression_decoding.py
@@ -51,8 +51,8 @@ DECODER_OUT_DIR = os.path.expanduser("~/data/multimodal_decoding/glm/")
 DISTANCE_METRICS = ['cosine']
 
 MASK_ANATOMICAL_LANGUAGE = "anatomical_lang"
-MASK_ANATOMICAL_VISUAL_LOW_LEVEL = "anatomical_visual_low_level_test5"
-MASK_ANATOMICAL_VISUAL_HIGH_LEVEL = "anatomical_visual_high_level_test8"
+MASK_ANATOMICAL_VISUAL_LOW_LEVEL = "anatomical_visual_low_level_test9"
+MASK_ANATOMICAL_VISUAL_HIGH_LEVEL = "anatomical_visual_high_level_test9"
 
 REGIONS_LOW_LEVEL_VISUAL = [
     'L G_and_S_occipital_inf',
@@ -73,6 +73,10 @@ REGIONS_LOW_LEVEL_VISUAL = [
     'R S_calcarine',
     'L G_cuneus',
     'R G_cuneus',
+    'L S_oc-temp_med_and_Lingual',
+    'R S_oc-temp_med_and_Lingual',
+    'L G_oc-temp_med-Lingual',
+    'R G_oc-temp_med-Lingual',
 ]
 
 REGIONS_HIGH_LEVEL_VISUAL = [
@@ -80,8 +84,6 @@ REGIONS_HIGH_LEVEL_VISUAL = [
     'R G_oc-temp_lat-fusifor',
     'L G_oc-temp_med-Parahip',
     'R G_oc-temp_med-Parahip',
-    'L S_oc-temp_med_and_Lingual',
-    'R S_oc-temp_med_and_Lingual',
     'L S_oc-temp_lat',
     'R S_oc-temp_lat',
     'L G_temporal_inf',
@@ -90,8 +92,6 @@ REGIONS_HIGH_LEVEL_VISUAL = [
     'R G_temporal_inf',
     'R G_temporal_middle',
     'R S_temporal_inf',
-    'L G_oc-temp_med-Lingual',
-    'R G_oc-temp_med-Lingual',
     'L S_parieto_occipital',
     'R S_parieto_occipital',
     'L S_collat_transv_post',
