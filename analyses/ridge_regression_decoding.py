55280dd7b176934045371f05a155bc50d6858e09
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-02-08 11:17:36 +0100

    lang roi test

--- analyses/ridge_regression_decoding.py
+++ analyses/ridge_regression_decoding.py
@@ -50,7 +50,7 @@ TEST_MODE_CHOICES = ['test', 'test_captions', 'test_images']
 DECODER_OUT_DIR = os.path.expanduser("~/data/multimodal_decoding/glm/")
 DISTANCE_METRICS = ['cosine']
 
-MASK_ANATOMICAL_LANGUAGE = "anatomical_lang"
+MASK_ANATOMICAL_LANGUAGE = "anatomical_lang_test"
 MASK_ANATOMICAL_OCCIPITAL_EXCLUSIVE = "anatomical_occipital_exclusive"
 MASK_ANATOMICAL_VISUAL_HIGH_LEVEL = "anatomical_visual_high_level"
 
@@ -104,8 +104,8 @@ REGIONS_LANGUAGE = [
     'L G_temp_sup-Plan_polar',  # Planum polare of the superior temporal gyrus
     'L G_and_S_subcentral',  # Subcentral gyrus (central operculum) and sulci:
     'L G_pariet_inf-Supramar',  # Supramarginal gyrus:
-    'L G_cingul-Post-dorsal',  # Posterior-dorsal part of the cingulate gyrus (dPCC)
-    'L G_cingul-Post-ventral',  # Posterior-ventral part of the cingulate gyrus (vPCC)
+    # 'L G_cingul-Post-dorsal',  # Posterior-dorsal part of the cingulate gyrus (dPCC)
+    # 'L G_cingul-Post-ventral',  # Posterior-ventral part of the cingulate gyrus (vPCC)
 ]
 
 
