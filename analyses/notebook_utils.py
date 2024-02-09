ee70b173fe0633d4a6d534ed9ce6c3bb0b74f89a
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-02-09 16:55:52 +0100

    Update notebooks

--- analyses/notebook_utils.py
+++ analyses/notebook_utils.py
@@ -3,14 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-
 from utils import FEATURES_DIR, RESULTS_DIR
 
 from glob import glob
 import pickle
 
-HP_KEYS = ["alpha", "model", "subject", "features", "training_mode", "testing_mode", "mask", "best_val_loss", "best_val_acc", "best_val_mse", "ensemble", "num_voxels"]
+HP_KEYS = ["alpha", "model", "subject", "features", "training_mode", "mask", "best_val_loss", "best_val_acc", "best_val_mse", "num_voxels"]
 METRIC_NAMES = {"acc_cosine": "pairwise_acc", "acc_cosine_captions": "pairwise_acc_captions", "acc_cosine_images": "pairwise_acc_images"}
+
 
 def add_avg_subject(df):
     df_mean = df.copy()
@@ -21,15 +21,15 @@ def add_avg_subject(df):
 def load_results_data(distance_metrics = ["cosine"]):
     results_root_dir = os.path.expanduser(f'~/data/multimodal_decoding/glm/')
     
-    metrics = ['val_loss', 'val_rsa', 'rsa', 'rsa_captions', 'rsa_images'] #, 'predictions', 'latents', 'stimulus_ids'
+    metrics = ['rsa', 'rsa_captions', 'rsa_images'] #, 'predictions', 'latents', 'stimulus_ids'
     for dist_metric in distance_metrics:
-        metrics.extend([f'acc_{dist_metric}', f'acc_{dist_metric}_captions', f'acc_{dist_metric}_images', f'val_acc_{dist_metric}'])         
+        metrics.extend([f'acc_{dist_metric}_captions', f'acc_{dist_metric}_images'])# f'acc_{dist_metric}'
     for metric in ["spearmanr", "pearsonr"]:
         for matrix_metric in ["spearmanr", "pearsonr"]: 
             metrics.append(f"rsa_{metric}_{matrix_metric}")
         
     data = []
-        
+
     result_files = sorted(glob(f"{results_root_dir}/*/*/*/results.p"))
     for result_file_path in result_files:        
         results = pickle.load(open(result_file_path, 'rb'))
@@ -39,7 +39,12 @@ def load_results_data(distance_metrics = ["cosine"]):
                 data_item = {k: value for k, value in results.items() if k in HP_KEYS}
                 data_item["metric"] = METRIC_NAMES[metric] if metric in METRIC_NAMES.keys() else metric
                 data_item["value"] = results[metric]
-                data.append(data_item)   
+                data.append(data_item)  
+        data_item = {k: value for k, value in results.items() if k in HP_KEYS}
+        data_item["metric"] = "pairwise_acc_mean"
+        data_item["value"] = (results["acc_cosine_captions"] + results["acc_cosine_images"]) / 2
+        data.append(data_item)
+
 
     df = pd.DataFrame.from_records(data)
 
@@ -51,6 +56,6 @@ def load_results_data(distance_metrics = ["cosine"]):
 
     df["model_feat"] = df.model + "_" + df.features
 
-    df = df[df.ensemble != True]
+    # df = df[df.ensemble != True]
 
     return df
