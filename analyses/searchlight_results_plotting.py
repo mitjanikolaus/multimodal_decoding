76606e05d8a3d3d72294423d2ff800dfade0d2d9
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-03-21 10:49:09 +0100

    update plotting

--- analyses/searchlight_results_plotting.py
+++ analyses/searchlight_results_plotting.py
@@ -21,6 +21,8 @@ from analyses.ridge_regression_decoding import TRAIN_MODE_CHOICES, FEATS_SELECT_
 
 from utils import VISION_MEAN_FEAT_KEY, RESULTS_DIR, SUBJECTS
 
+METRIC_MIN_DIFF_BOTH_MODALITIES = 'min(captions_agno - captions_specific, imgs_agno - imgs_specific)'
+
 SEARCHLIGHT_OUT_DIR = os.path.expanduser("~/data/multimodal_decoding/searchlight/")
 
 COLORBAR_MAX = 0.85
@@ -197,12 +199,12 @@ def run(args):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=RuntimeWarning)
             t_values[hemi]['mean(captions_agno - captions_specific, imgs_agno - imgs_specific)'] = np.nanmean((t_values[hemi]['captions_agno - captions_specific'], t_values[hemi]['imgs_agno - imgs_specific']), axis=0)
-            t_values[hemi]['min(captions_agno - captions_specific, imgs_agno - imgs_specific)'] = np.nanmin((t_values[hemi]['captions_agno - captions_specific'], t_values[hemi]['imgs_agno - imgs_specific']), axis=0)
+            t_values[hemi][METRIC_MIN_DIFF_BOTH_MODALITIES] = np.nanmin((t_values[hemi]['captions_agno - captions_specific'], t_values[hemi]['imgs_agno - imgs_specific']), axis=0)
 
-    # plot group-level t-values with low threshold (=0.5)
+    # plot group-level t-values with low threshold (=0)
     metrics = ['imgs_agno - imgs_specific',
                'captions_agno - captions_specific',
-               'min(captions_agno - captions_specific, imgs_agno - imgs_specific)']
+               METRIC_MIN_DIFF_BOTH_MODALITIES]
     scores = t_values
     fig = plt.figure(figsize=(5 * len(VIEWS), len(metrics) * 2))
     subfigs = fig.subfigures(nrows=len(metrics), ncols=1)
@@ -222,7 +224,7 @@ def run(args):
                         cbar_max = min(np.nanmax(scores_hemi), 99)
                         cbar_min = max(np.nanmin(scores_hemi), -99)
 
-                    display = plotting.plot_surf_stat_map(
+                    plotting.plot_surf_stat_map(
                         infl_mesh,
                         scores_hemi,
                         hemi=hemi,
@@ -230,17 +232,17 @@ def run(args):
                         bg_map=fsaverage[f"sulc_{hemi}"],
                         axes=axes[i * 2 + j],
                         colorbar=True if axes[i * 2 + j] == axes[-1] else False,
-                        threshold=0.5,  # for 5 degrees of freedom (6 subjects): 2.015 for p<0.05 (one-sided)
-                        vmax=99 if CHANCE_VALUES[metric] == 0.5 else cbar_max,
-                        vmin=0.0 if CHANCE_VALUES[metric] == 0.5 else -cbar_max,
-                        cmap="hot" if CHANCE_VALUES[metric] == 0.5 else "cold_hot",
-                        symmetric_cbar=True,  # if CHANCE_VALUES[metric] == 0.5 else True,
+                        threshold=0.01 if metric == METRIC_MIN_DIFF_BOTH_MODALITIES else 1.5,
+                        vmax=cbar_max,
+                        vmin=0 if metric == METRIC_MIN_DIFF_BOTH_MODALITIES else -cbar_max,
+                        cmap="hot" if metric == METRIC_MIN_DIFF_BOTH_MODALITIES else "cold_hot",
+                        symmetric_cbar=False if metric == METRIC_MIN_DIFF_BOTH_MODALITIES else True,
                     )
                     axes[i * 2 + j].set_title(f"{hemi} {view}", y=0.85, fontsize=10)
                 else:
                     axes[i * 2 + j].axis('off')
 
-    title = f"{args.model}_{args.mode}_group_level_t_values_threshold_0.5"
+    title = f"{args.model}_{args.mode}_group_level_t_values_threshold_0.01"
     # fig.suptitle(title)
     # fig.tight_layout()
     fig.subplots_adjust(left=0, right=0.85, bottom=0, wspace=-0.1, hspace=0, top=1)
@@ -253,7 +255,7 @@ def run(args):
     # plot group-level t-values with threshold = 2.015
     metrics = ['imgs_agno - imgs_specific',
                'captions_agno - captions_specific',
-               'min(captions_agno - captions_specific, imgs_agno - imgs_specific)']
+               METRIC_MIN_DIFF_BOTH_MODALITIES]
     scores = t_values
     fig = plt.figure(figsize=(5 * len(VIEWS), len(metrics) * 2))
     subfigs = fig.subfigures(nrows=len(metrics), ncols=1)
@@ -282,10 +284,10 @@ def run(args):
                         axes=axes[i * 2 + j],
                         colorbar=True if axes[i * 2 + j] == axes[-1] else False,
                         threshold=2.015, # for 5 degrees of freedom (6 subjects): 2.015 for p<0.05 (one-sided)
-                        vmax=99 if CHANCE_VALUES[metric] == 0.5 else cbar_max,
-                        vmin=0.0 if CHANCE_VALUES[metric] == 0.5 else -cbar_max,
-                        cmap="hot" if CHANCE_VALUES[metric] == 0.5 else "cold_hot",
-                        symmetric_cbar=True,    # if CHANCE_VALUES[metric] == 0.5 else True,
+                        vmax=cbar_max,
+                        vmin=0 if metric == METRIC_MIN_DIFF_BOTH_MODALITIES else -cbar_max,
+                        cmap="hot" if metric == METRIC_MIN_DIFF_BOTH_MODALITIES else "cold_hot",
+                        symmetric_cbar=False if metric == METRIC_MIN_DIFF_BOTH_MODALITIES else True,
                     )
                     axes[i * 2 + j].set_title(f"{hemi} {view}", y=0.85, fontsize=10)
                 else:
@@ -352,87 +354,89 @@ def run(args):
     plt.savefig(results_searchlight, dpi=300, bbox_inches='tight')
     plt.close()
 
-    # per-subject plots
-    metrics = ["captions", "images", "min(imgs,captions)",
-               'imgs_agno - imgs_specific',
-               'captions_agno - captions_specific']
-    print("\n\nCreating per-subject plots..")
-    for scores in tqdm(per_subject_scores):
-        fig = plt.figure(figsize=(5 * len(VIEWS), len(metrics) * 2))
-        subfigs = fig.subfigures(nrows=len(metrics), ncols=1)
-        fsaverage = datasets.fetch_surf_fsaverage(mesh=args.resolution)
+    if args.per_subject_plots:
+        # per-subject plots
+        metrics = ["captions", "images", "min(imgs,captions)",
+                   'imgs_agno - imgs_specific',
+                   'captions_agno - captions_specific']
+        print("\n\nCreating per-subject plots..")
+        for scores in tqdm(per_subject_scores):
+            fig = plt.figure(figsize=(5 * len(VIEWS), len(metrics) * 2))
+            subfigs = fig.subfigures(nrows=len(metrics), ncols=1)
+            fsaverage = datasets.fetch_surf_fsaverage(mesh=args.resolution)
 
-        for subfig, metric in zip(subfigs, metrics):
-            subfig.suptitle(f'{metric}', x=0, horizontalalignment="left")
-            axes = subfig.subplots(nrows=1, ncols=2 * len(VIEWS), subplot_kw={'projection': '3d'})
-            cbar_max = None
-            cbar_min = None
-            for i, view in enumerate(VIEWS):
-                for j, hemi in enumerate(['left', 'right']):
-                    if metric in scores[hemi].keys():
-                        scores_hemi = scores[hemi][metric]
+            for subfig, metric in zip(subfigs, metrics):
+                subfig.suptitle(f'{metric}', x=0, horizontalalignment="left")
+                axes = subfig.subplots(nrows=1, ncols=2 * len(VIEWS), subplot_kw={'projection': '3d'})
+                cbar_max = None
+                cbar_min = None
+                for i, view in enumerate(VIEWS):
+                    for j, hemi in enumerate(['left', 'right']):
+                        if metric in scores[hemi].keys():
+                            scores_hemi = scores[hemi][metric]
 
-                        infl_mesh = fsaverage[f"infl_{hemi}"]
-                        if cbar_max is None:
-                            cbar_max = np.nanmax(scores_hemi)
-                            cbar_min = np.nanmin(scores_hemi)
-                        # print(f" | max score: {cbar_max:.2f}")
+                            infl_mesh = fsaverage[f"infl_{hemi}"]
+                            if cbar_max is None:
+                                cbar_max = np.nanmax(scores_hemi)
+                                cbar_min = np.nanmin(scores_hemi)
+                            # print(f" | max score: {cbar_max:.2f}")
 
-                        # destrieux_atlas = datasets.fetch_atlas_surf_destrieux()
-                        # parcellation = destrieux_atlas['map_right']
-                        # parcellation = destrieux_atlas[f'maps']
-                        # parcellation_surf = surface.vol_to_surf(parcellation, fsaverage[f'pial_{hemi}'], interpolation="nearest", radius=5).astype(int) #TODO
+                            # destrieux_atlas = datasets.fetch_atlas_surf_destrieux()
+                            # parcellation = destrieux_atlas['map_right']
+                            # parcellation = destrieux_atlas[f'maps']
+                            # parcellation_surf = surface.vol_to_surf(parcellation, fsaverage[f'pial_{hemi}'], interpolation="nearest", radius=5).astype(int) #TODO
 
-                        # these are the regions we want to outline
-                        # regions_dict = {'L G_pariet_inf-Angular': 'left angular gyrus'}
-                        # regions_dict = {b'G_postcentral': 'Postcentral gyrus',
-                        #                 b'G_precentral': 'Precentral gyrus'}
+                            # these are the regions we want to outline
+                            # regions_dict = {'L G_pariet_inf-Angular': 'left angular gyrus'}
+                            # regions_dict = {b'G_postcentral': 'Postcentral gyrus',
+                            #                 b'G_precentral': 'Precentral gyrus'}
 
-                        # get indices in atlas for these labels
-                        # regions_indices = [
-                        #     [i for i, l in destrieux_atlas['labels'] if l == region][0]
-                        #     for region in regions_dict
-                        # ]
-                        # regions_indices = [
-                        #     np.where(np.array(destrieux_atlas['labels']) == region)[0][0]
-                        #     for region in regions_dict
-                        # ]
-                        # colors = ['g', 'b']
-                        #
-                        # labels = list(regions_dict.values())
+                            # get indices in atlas for these labels
+                            # regions_indices = [
+                            #     [i for i, l in destrieux_atlas['labels'] if l == region][0]
+                            #     for region in regions_dict
+                            # ]
+                            # regions_indices = [
+                            #     np.where(np.array(destrieux_atlas['labels']) == region)[0][0]
+                            #     for region in regions_dict
+                            # ]
+                            # colors = ['g', 'b']
+                            #
+                            # labels = list(regions_dict.values())
 
-                        plotting.plot_surf_stat_map(
-                            infl_mesh,
-                            scores_hemi,
-                            hemi=hemi,
-                            view=view,
-                            bg_map=fsaverage[f"sulc_{hemi}"],
-                            axes=axes[i * 2 + j],
-                            colorbar=True if axes[i * 2 + j] == axes[-1] else False,
-                            threshold=COLORBAR_THRESHOLD_MIN if cbar_min >= 0 else COLORBAR_DIFFERENCE_THRESHOLD_MIN,
-                            vmax=COLORBAR_MAX if cbar_min >= 0 else None,  # cbar_max,
-                            vmin=0.5 if cbar_min >= 0 else None,
-                            cmap="hot" if cbar_min >= 0 else "cold_hot",
-                            symmetric_cbar=True if cbar_min < 0 else "auto",
-                        )
-                        axes[i * 2 + j].set_title(f"{hemi} {view}", y=0.85, fontsize=10)
+                            plotting.plot_surf_stat_map(
+                                infl_mesh,
+                                scores_hemi,
+                                hemi=hemi,
+                                view=view,
+                                bg_map=fsaverage[f"sulc_{hemi}"],
+                                axes=axes[i * 2 + j],
+                                colorbar=True if axes[i * 2 + j] == axes[-1] else False,
+                                threshold=COLORBAR_THRESHOLD_MIN if cbar_min >= 0 else COLORBAR_DIFFERENCE_THRESHOLD_MIN,
+                                vmax=COLORBAR_MAX if cbar_min >= 0 else None,  # cbar_max,
+                                vmin=0.5 if cbar_min >= 0 else None,
+                                cmap="hot" if cbar_min >= 0 else "cold_hot",
+                                symmetric_cbar=True if cbar_min < 0 else "auto",
+                            )
+                            axes[i * 2 + j].set_title(f"{hemi} {view}", y=0.85, fontsize=10)
 
-                        # plotting.plot_surf_contours(infl_mesh, parcellation_surf, labels=labels,
-                        #                             levels=regions_indices, axes=row_axes[i*2+j],
-                        #                             legend=True,
-                        #                             colors=colors)
-                    else:
-                        axes[i * 2 + j].axis('off')
+                            # plotting.plot_surf_contours(infl_mesh, parcellation_surf, labels=labels,
+                            #                             levels=regions_indices, axes=row_axes[i*2+j],
+                            #                             legend=True,
+                            #                             colors=colors)
+                        else:
+                            axes[i * 2 + j].axis('off')
 
-        title = f"{args.model}_{args.mode}_{scores['subject']}"
-        # fig.suptitle(title)
-        # fig.tight_layout()
-        fig.subplots_adjust(left=0, right=0.85, bottom=0, wspace=-0.1, hspace=0, top=1)
-        title += f"_alpha_{str(alpha)}"
-        results_searchlight = os.path.join(RESULTS_DIR, "searchlight", args.resolution, f"{title}.png")
-        os.makedirs(os.path.dirname(results_searchlight), exist_ok=True)
-        plt.savefig(results_searchlight, dpi=300, bbox_inches='tight')
-        plt.close()
+            title = f"{args.model}_{args.mode}_{scores['subject']}"
+            # fig.suptitle(title)
+            # fig.tight_layout()
+            fig.subplots_adjust(left=0, right=0.85, bottom=0, wspace=-0.1, hspace=0, top=1)
+            title += f"_alpha_{str(alpha)}"
+            results_searchlight = os.path.join(RESULTS_DIR, "searchlight", args.resolution, f"{title}.png")
+            os.makedirs(os.path.dirname(results_searchlight), exist_ok=True)
+            plt.savefig(results_searchlight, dpi=300, bbox_inches='tight')
+            plt.close()
+
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -440,6 +444,7 @@ def get_args():
     parser.add_argument("--model", type=str, default='vilt')
     parser.add_argument("--resolution", type=str, default='fsaverage7')
     parser.add_argument("--mode", type=str, default='n_neighbors_100')
+    parser.add_argument("--per-subject-plots", default=False, action=argparse.BooleanOptionalAction)
 
     return parser.parse_args()
 
