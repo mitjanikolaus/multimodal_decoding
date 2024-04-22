e5796bcb9db690fdb6d8f21d84b48c9d033bb803
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-04-22 11:51:38 +0200

    adapt file names

--- analyses/searchlight_results_plotting.py
+++ analyses/searchlight_results_plotting.py
@@ -58,7 +58,7 @@ def plot_test_statistics(test_statistics, args, results_path, filename_suffix=""
                     cmap="hot",
                 )
                 axes[i * 2 + j].set_title(f"{hemi} {view}", y=0.85, fontsize=10)
-    title = f"{args.model}_{args.mode}_group_level_test_stats{filename_suffix}"
+    title = f"{args.model}_{args.mode}_metric_{METRIC_CODES[args.metric]}_test_stats{filename_suffix}"
     # fig.suptitle(title)
     # fig.tight_layout()
     fig.subplots_adjust(left=0, right=0.85, bottom=0, wspace=-0.1, hspace=0, top=1)
@@ -127,7 +127,7 @@ def run(args):
                 symmetric_cbar=False,
             )
             axes[i * 2 + j].set_title(f"{hemi} {view}", y=0.85, fontsize=10)
-    title = f"{args.model}_{args.mode}_group_level_p_values"
+    title = f"{args.model}_{args.mode}_metric_{METRIC_CODES[args.metric]}_p_values"
     fig.subplots_adjust(left=0, right=0.85, bottom=0, wspace=-0.1, hspace=0, top=1)
     results_searchlight = os.path.join(results_path, f"{title}.png")
     plt.savefig(results_searchlight, dpi=300, bbox_inches='tight')
@@ -169,7 +169,7 @@ def run(args):
                     symmetric_cbar=False if CHANCE_VALUES[metric] == 0.5 else True,
                 )
                 axes[i * 2 + j].set_title(f"{hemi} {view}", y=0.85, fontsize=10)
-    title = f"{args.model}_{args.mode}_group_level_pairwise_acc"
+    title = f"{args.model}_{args.mode}_metric_{METRIC_CODES[args.metric]}_pairwise_acc"
     fig.subplots_adjust(left=0, right=0.85, bottom=0, wspace=-0.1, hspace=0, top=1)
     results_searchlight = os.path.join(results_path, f"{title}.png")
     plt.savefig(results_searchlight, dpi=300, bbox_inches='tight')
