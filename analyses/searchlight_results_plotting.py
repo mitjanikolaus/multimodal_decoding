b380c99ffbd9ed4e58cd79d58fcfded03bfba49a
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-04-22 16:49:16 +0200

    default values are floats

--- analyses/searchlight_results_plotting.py
+++ analyses/searchlight_results_plotting.py
@@ -316,8 +316,8 @@ def get_args():
     parser.add_argument("--smoothing-iterations", type=int, default=0)
 
     parser.add_argument("--tfce", action="store_true")
-    parser.add_argument("--tfce-h", type=float, default=2)
-    parser.add_argument("--tfce-e", type=float, default=1)
+    parser.add_argument("--tfce-h", type=float, default=2.0)
+    parser.add_argument("--tfce-e", type=float, default=1.0)
 
     parser.add_argument("--metric", type=str, default=METRIC_MIN_DIFF_BOTH_MODALITIES)
 
