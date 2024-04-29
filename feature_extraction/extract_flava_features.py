e7dcf478d08f2a9e1bab176e02ff0f1e8802bfbc
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-04-29 13:57:09 +0200

    flava mulitmodal feats

--- feature_extraction/extract_flava_features.py
+++ feature_extraction/extract_flava_features.py
@@ -1,12 +1,17 @@
 import os
+import pickle
 
 import torch
 from torch import nn
+from tqdm import tqdm
 
 from transformers import FlavaModel, FlavaProcessor
 
 from feature_extraction.feat_extraction_utils import FeatureExtractor
 from PIL import Image
+
+from utils import LANG_FEAT_KEY, VISION_MEAN_FEAT_KEY, VISION_CLS_FEAT_KEY, MULTIMODAL_FEAT_KEY, \
+    model_features_file_path
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
 # os.environ["CUDA_VISIBLE_DEVICES"] = "1"
@@ -41,7 +46,43 @@ class FlavaFeatureExtractor(FeatureExtractor):
 
         feats_vision_mean = image_embeddings[:, 1:].mean(axis=1)
 
-        return text_embedding, feats_vision_mean, image_embedding
+        feats_multi = outputs.multimodal_output.pooler_output
+
+        return text_embedding, feats_vision_mean, image_embedding, feats_multi
+
+    def extract_features(self):
+        all_feats = dict()
+        for ids, captions, img_paths in tqdm(self.dloader):
+            ids = [id.item() for id in ids]
+            language_feats_batch, vision_mean_feats_batch, vision_cls_feats_batch, multi_feats_batch = self.extract_features_from_batch(
+                ids, captions, img_paths)
+            if language_feats_batch is None:
+                language_feats_batch = [None] * len(ids)
+            else:
+                language_feats_batch = language_feats_batch.cpu().numpy()
+            if vision_mean_feats_batch is None:
+                vision_mean_feats_batch = [None] * len(ids)
+            else:
+                vision_mean_feats_batch = vision_mean_feats_batch.cpu().numpy()
+            if vision_cls_feats_batch is None:
+                vision_cls_feats_batch = [None] * len(ids)
+            else:
+                vision_cls_feats_batch = vision_cls_feats_batch.cpu().numpy()
+            if multi_feats_batch is None:
+                multi_feats_batch = [None] * len(ids)
+            else:
+                multi_feats_batch = multi_feats_batch.cpu().numpy()
+            for id, feats_lang, feats_vision_mean, feats_vision_cls, feats_multi in zip(ids,
+                                                                                        language_feats_batch,
+                                                                                        vision_mean_feats_batch,
+                                                                                        vision_cls_feats_batch,
+                                                                                        multi_feats_batch):
+                all_feats[id] = {LANG_FEAT_KEY: feats_lang, VISION_MEAN_FEAT_KEY: feats_vision_mean,
+                                 VISION_CLS_FEAT_KEY: feats_vision_cls, MULTIMODAL_FEAT_KEY: feats_multi}
+
+        path_out = model_features_file_path(self.model_name)
+        os.makedirs(os.path.dirname(path_out), exist_ok=True)
+        pickle.dump(all_feats, open(path_out, 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == "__main__":
@@ -58,6 +99,3 @@ if __name__ == "__main__":
     #
     # extractor = FlavaFeatureExtractor(model, processor, "random-flava", BATCH_SIZE, device)
     # extractor.extract_features()
-
-
-
