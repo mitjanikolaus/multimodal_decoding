3e7c02e596210b4634031503c9846c575f7e63e1
mitja <mitja.nikolaus@posteo.de>
2024-06-02 13:05:51 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='multimodal_decoding',
-    version='',
+    version='0.1',
     packages=find_packages(),
     url='',
     license='',
