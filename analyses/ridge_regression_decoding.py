203ab70148d8487d26995a53c37c2a369a813734
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-06-03 10:22:35 +0200

    bridgetower default feats

--- analyses/ridge_regression_decoding.py
+++ analyses/ridge_regression_decoding.py
@@ -215,7 +215,7 @@ def get_default_features(model_name):
 def get_default_vision_features(model_name):
     vision_feats = VISION_MEAN_FEAT_KEY
     if model_name.startswith("flava") or model_name.startswith("imagebind") or model_name.startswith(
-            "clip") or model_name.startswith("bridgetower"):
+            "clip"):
         vision_feats = VISION_CLS_FEAT_KEY
     elif model_name.startswith("blip"):
         vision_feats = VISION_MEAN_FEAT_KEY
