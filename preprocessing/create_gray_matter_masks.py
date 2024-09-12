4a66d6fef98975ace62197e881e6b207c6827325
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-09-12 16:42:02 +0200

    args for graymatter mask creation

--- preprocessing/create_gray_matter_masks.py
+++ preprocessing/create_gray_matter_masks.py
@@ -3,20 +3,21 @@ import argparse
 import os
 import nibabel as nib
 
-from utils import SUBJECTS, FREESURFER_BASE_DIR, FMRI_RAW_DATA_DIR, FMRI_PREPROCESSED_DATA_DIR
+from utils import SUBJECTS, FREESURFER_BASE_DIR, FMRI_PREPROCESSED_DATA_DIR, FMRI_ANATOMICAL_DATA_DIR
 
 
 def convert_mask_to_mni(mask_file, subject, out_file):
     print('Converting mask to MNI space')
     reg_file = f'{FREESURFER_BASE_DIR}/regfiles/{subject}/spm2fs.change-name.lta'
+    os.makedirs(os.path.dirname(out_file), exist_ok=True)
     conv_cmd = f'mri_vol2vol --mov "{mask_file}" --reg "{reg_file}" --o "{out_file}" --tal --talres 2 --interp nearest'
     result_code = os.system(conv_cmd)
     if result_code != 0:
         raise RuntimeError(f"mri_vol2vol failed with error code {result_code}")
 
 
-def get_graymatter_mask_path(subject):
-    mni_mask_image_path = os.path.join(FMRI_PREPROCESSED_DATA_DIR, 'graymatter_masks', subject, f'mask.nii')
+def get_graymatter_mask_path(subject, preprocessed_data_dir=FMRI_PREPROCESSED_DATA_DIR):
+    mni_mask_image_path = os.path.join(preprocessed_data_dir, 'graymatter_masks', subject, f'mask.nii')
     return mni_mask_image_path
 
 
@@ -27,7 +28,7 @@ def run(args):
         print(subject)
 
         print('Creating mask')
-        c1_image_path = os.path.join(FMRI_RAW_DATA_DIR, 'corrected_anat', subject, f'c1{subject}_ses-01_run-01_T1W.nii')
+        c1_image_path = os.path.join(args.anatomical_data_dir, subject, f'c1{subject}_ses-01_run-01_T1W.nii')
         c1_img = nib.load(c1_image_path)
         c1_img_data = c1_img.get_fdata()
         data_masked = c1_img_data.copy()
@@ -38,12 +39,15 @@ def run(args):
         os.makedirs(os.path.dirname(mask_image_path), exist_ok=True)
         nib.save(mask_img, mask_image_path)
 
-        mni_mask_image_path = get_graymatter_mask_path(subject)
+        mni_mask_image_path = get_graymatter_mask_path(subject, args.preprocessed_data_dir)
         convert_mask_to_mni(mask_image_path, subject, mni_mask_image_path)
 
 
 def get_args():
     parser = argparse.ArgumentParser()
+
+    parser.add_argument("--anatomical-data-dir", type=str, default=FMRI_ANATOMICAL_DATA_DIR)
+    parser.add_argument("--preprocessed-data-dir", type=str, default=FMRI_PREPROCESSED_DATA_DIR)
 
     parser.add_argument("--subjects", type=str, nargs='+', default=SUBJECTS)
 
