7fc80e8d333d076b75be4bb55379b7ae27d38713
mitja <mitja.nikolaus@posteo.de>
2024-09-14 10:28:33 +0200

    create graymatter masks with dtype int

--- preprocessing/create_gray_matter_masks.py
+++ preprocessing/create_gray_matter_masks.py
@@ -5,8 +5,9 @@ import nibabel as nib
 from utils import SUBJECTS, FREESURFER_BASE_DIR, FMRI_RAW_DATA_DIR, FMRI_PREPROCESSED_DATA_DIR
 
 
-def get_graymatter_mask_path(subject, mni=False):
+def get_graymatter_mask_path(subject, mni=False, downsampled=True):
     file_suffix = "_mni" if mni else "_orig"
+    file_suffix += "_downsampled" if downsampled else ""
 
     mni_mask_image_path = os.path.join(
         FMRI_PREPROCESSED_DATA_DIR, 'graymatter_masks', subject, f'mask{file_suffix}.nii'
@@ -26,11 +27,13 @@ def run(args):
         c1_img_data = c1_img.get_fdata()
         data_masked = c1_img_data.copy()
         data_masked[data_masked > 0] = 1
+        data_masked[data_masked < 1] = 0
+        data_masked = data_masked.astype(int)
         print(f"Subject-space gray matter mask size: {data_masked.sum()}")
 
         mask_img = nib.Nifti1Image(data_masked, c1_img.affine, c1_img.header)
 
-        mask_image_path = get_graymatter_mask_path(subject, mni=False)
+        mask_image_path = get_graymatter_mask_path(subject, mni=False, downsampled=False)
         os.makedirs(os.path.dirname(mask_image_path), exist_ok=True)
         nib.save(mask_img, mask_image_path)
 
