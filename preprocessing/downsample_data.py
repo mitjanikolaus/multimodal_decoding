66d3aaa920641a9932012afb264c7e98d1d4a7cb
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-09-17 15:54:33 +0200

    update downsampling

--- preprocessing/downsample_data.py
+++ preprocessing/downsample_data.py
@@ -3,7 +3,7 @@ import os
 from glob import glob
 
 import nibabel as nib
-from nibabel import affines
+import numpy as np
 from nilearn.image import resample_img
 from tqdm import tqdm
 
@@ -13,6 +13,18 @@ from utils import SUBJECTS, FMRI_PREPROCESSED_DATA_DIR, nipype_subject_id
 DIMS_ORIGINAL_SPACE = (170, 240, 240)
 DIMS_MNI_305_2MM = (76, 76, 93)
 VOXEL_SIZE_MNI_305_2MM = (2, 2, 2)
+MNI_305_2MM_AFFINE = np.array([[-2, 0, 0, 74], [0, 0, 2, -109], [0, -2, 0, 85], [0, 0, 0, 1]])
+
+
+def downsample_img(path, path_out, interpolation='continuous'):
+    img = nib.load(path)
+    assert img.shape[:3] == DIMS_ORIGINAL_SPACE
+
+    downsampled_img = resample_img(
+        img, target_shape=DIMS_MNI_305_2MM, target_affine=MNI_305_2MM_AFFINE, interpolation=interpolation
+    )
+
+    nib.save(downsampled_img, path_out)
 
 
 def downsample_graymatter_masks(args):
@@ -21,20 +33,8 @@ def downsample_graymatter_masks(args):
         print(subject)
 
         graymatter_mask_path = get_graymatter_mask_path(subject, downsampled=False)
-        mask = nib.load(graymatter_mask_path)
-        assert mask.shape[:3] == DIMS_ORIGINAL_SPACE
-
-        target_affine = affines.rescale_affine(
-            mask.affine, shape=DIMS_ORIGINAL_SPACE, zooms=VOXEL_SIZE_MNI_305_2MM, new_shape=DIMS_MNI_305_2MM
-        )
-
-        resampled_mask = resample_img(
-            mask, target_shape=DIMS_MNI_305_2MM, target_affine=target_affine, interpolation='nearest'
-        )
-
         out_path = get_graymatter_mask_path(subject, downsampled=True)
-
-        nib.save(resampled_mask, out_path)
+        downsample_img(graymatter_mask_path, out_path, interpolation='nearest')
 
 
 def downsample_functional_scans(args):
@@ -49,41 +49,11 @@ def downsample_functional_scans(args):
             )
         )
         for img_path in tqdm(img_paths):
-            img = nib.load(img_path)
-            assert img.shape[:3] == DIMS_ORIGINAL_SPACE
-
-            target_affine = affines.rescale_affine(
-                img.affine, shape=DIMS_ORIGINAL_SPACE, zooms=VOXEL_SIZE_MNI_305_2MM, new_shape=DIMS_MNI_305_2MM
-            )
-
-            resampled_img = resample_img(img, target_shape=DIMS_MNI_305_2MM, target_affine=target_affine)
-
             out_path = img_path.replace("coregister", "coregister_downsampled")
+            os.makedirs(os.path.dirname(out_path), exist_ok=True)
             assert out_path != img_path
 
-            os.makedirs(os.path.dirname(out_path), exist_ok=True)
-            nib.save(resampled_img, out_path)
-
-            # from nilearn import plotting
-            # template = load_mni152_template(resolution=2)
-            # fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(10, 5))
-            # plotting.plot_stat_map(
-            #     img,
-            #     bg_img=template,
-            #     cut_coords=(36, -27, 66),
-            #     threshold=100,
-            #     title="img in original resolution",
-            #     axes=axes[0],
-            # )
-            # plotting.plot_stat_map(
-            #     resampled_img,
-            #     bg_img=template,
-            #     cut_coords=(36, -27, 66),
-            #     threshold=100,
-            #     title="Resampled img",
-            #     axes=axes[1],
-            # )
-            # plt.show()
+            downsample_img(img_path, out_path)
 
 
 def get_args():
