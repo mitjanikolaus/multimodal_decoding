daacb91e950149184ce25c6a2d3cb1d944e95ea4
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-10-31 16:56:11 +0100

    join masks scripts with permutation testing

--- analyses/searchlight/searchlight_permutation_testing.py
+++ analyses/searchlight/searchlight_permutation_testing.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 import hashlib
 import itertools
 import math
@@ -23,7 +24,8 @@ from analyses.ridge_regression_decoding import MOD_SPECIFIC_CAPTIONS, MOD_SPECIF
 from analyses.searchlight.searchlight import SEARCHLIGHT_OUT_DIR, METRIC_MIN_DIFF_BOTH_MODALITIES, \
     METRIC_DIFF_CAPTIONS, METRIC_DIFF_IMAGES, METRIC_MIN, METRIC_CAPTIONS, METRIC_IMAGES, \
     SEARCHLIGHT_PERMUTATION_TESTING_RESULTS_DIR
-from utils import SUBJECTS, HEMIS, DEFAULT_RESOLUTION
+from analyses.searchlight.searchlight_results_maps import create_gifti_results_maps
+from utils import SUBJECTS, HEMIS, DEFAULT_RESOLUTION, FS_HEMI_NAMES, export_to_gifti
 
 DEFAULT_N_JOBS = 10
 
@@ -733,6 +735,65 @@ def create_null_distribution(args):
         pickle.dump(tfce_values, open(tfce_values_null_distribution_path, 'wb'))
 
 
+def create_masks(args):
+    print("Creating gifti results masks")
+    p_values_path = os.path.join(permutation_results_dir(args), f"p_values{get_hparam_suffix(args)}.p")
+    masks_path = os.path.join(os.path.dirname(p_values_path), "masks")
+    os.makedirs(masks_path, exist_ok=True)
+    p_values_gifti_path = os.path.join(os.path.dirname(p_values_path), "p_values_gifti")
+    os.makedirs(p_values_gifti_path, exist_ok=True)
+
+    p_values = pickle.load(open(p_values_path, "rb"))
+
+    # transform to plottable magnitudes:
+    log_10_p_values = copy.deepcopy(p_values)
+    log_10_p_values['left'][~np.isnan(p_values['left'])] = - np.log10(p_values['left'][~np.isnan(p_values['left'])])
+    log_10_p_values['right'][~np.isnan(p_values['right'])] = - np.log10(p_values['right'][~np.isnan(p_values['right'])])
+
+    for hemi in HEMIS:
+        path_out = os.path.join(permutation_results_dir(args), f"p_values_{FS_HEMI_NAMES[hemi]}.gii")
+        export_to_gifti(log_10_p_values[hemi], path_out)
+
+    # tfce values
+    tfce_values_path = os.path.join(permutation_results_dir(args), f"tfce_values{get_hparam_suffix(args)}.p")
+    tfce_values = pickle.load(open(tfce_values_path, "rb"))
+
+    for hemi in HEMIS:
+        path_out = os.path.join(permutation_results_dir(args), f"tfce_values_{FS_HEMI_NAMES[hemi]}.gii")
+        export_to_gifti(tfce_values[hemi][METRIC_MIN], path_out)
+
+    # p value masks
+    masks = copy.deepcopy(p_values)
+    for hemi in HEMIS:
+        masks[hemi][p_values[hemi] < args.p_value_threshold] = 1
+        masks[hemi][p_values[hemi] >= args.p_value_threshold] = 0
+        masks[hemi][np.isnan(p_values[hemi])] = 0
+        masks[hemi] = masks[hemi].astype(np.uint8)
+
+    path_out = os.path.join(masks_path, f"p_values_thresh_{args.p_value_threshold}.p")
+    pickle.dump(masks, open(path_out, mode='wb'))
+
+    edge_lengths = get_edge_lengths_dicts_based_on_edges(args.resolution)
+    for hemi in HEMIS:
+        print(hemi)
+        results = calc_clusters(masks[hemi], threshold=1e-8, edge_lengths=edge_lengths[hemi], return_clusters=True)
+        clusters = results['clusters']
+        clusters.sort(key=len, reverse=True)
+        for i, cluster in enumerate(clusters[:10]):
+            print(f"{i}: Cluster of size {len(cluster)}")
+            cluster_map = np.repeat(np.nan, log_10_p_values[hemi].shape)
+            cluster_map[list(cluster)] = log_10_p_values[hemi][list(cluster)]
+
+            fname = f"thresh_{args.p_value_threshold}_{FS_HEMI_NAMES[hemi]}_cluster_{i}.gii"
+            path_out = os.path.join(p_values_gifti_path, fname)
+            export_to_gifti(cluster_map, path_out)
+
+            cluster_mask = {h: np.zeros_like(cluster_map, dtype=np.uint8) for h in HEMIS}
+            cluster_mask[hemi][list(cluster)] = 1
+            path_out = os.path.join(masks_path, f"p_values_thresh_{args.p_value_threshold}_{hemi}_cluster_{i}.p")
+            pickle.dump(cluster_mask, open(path_out, mode='wb'))
+
+
 def get_args():
     parser = argparse.ArgumentParser()
 
@@ -760,6 +821,8 @@ def get_args():
 
     parser.add_argument("--metric", type=str, default=METRIC_MIN)
 
+    parser.add_argument("--p-value-threshold", type=float, default=0.01)
+
     return parser.parse_args()
 
 
@@ -767,7 +830,6 @@ if __name__ == "__main__":
     os.makedirs(SEARCHLIGHT_PERMUTATION_TESTING_RESULTS_DIR, exist_ok=True)
     args = get_args()
 
-    create_gifti_results_maps(args)
-
     create_null_distribution(args)
     calc_test_statistics(args)
+    create_masks(args)
