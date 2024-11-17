fix for numerical stability

analyses/searchlight/searchlight_permutation_testing.py
@@ -379,14 +379,28 @@ def calc_clusters(scores, threshold, edge_lengths=None, return_clusters=True,
     return result_dict
 
 
-def calc_image_t_values(data, popmean, use_tqdm=False, t_vals_cache=None, precision=2):
+def calc_t_value(values, popmean, epsilon=1e-8):
+    # use heuristic (mean needs to be greater than popmean) to speed up calculation
+    values_no_nan = values[~np.isnan(values)]
+    if values_no_nan.mean() > popmean:
+        t_val = stats.ttest_1samp(values_no_nan, popmean=popmean, alternative="greater")[0]
+        if np.isinf(t_val):
+            # Add/subtract epsilon for numerical stability
+            values_no_nan[0] = values_no_nan + epsilon
+            values_no_nan[-1] = values_no_nan - epsilon
+            t_val = stats.ttest_1samp(values_no_nan, popmean=popmean, alternative="greater")[0]
+            print(f"corrected t val: {t_val}")
+        return t_val
+    else:
+        return 0
+
+
+def calc_image_t_values(data, popmean, use_tqdm=False, t_vals_cache=None, precision=2, epsilon=1e-8):
     data = data.round(precision)
     iterator = tqdm(data.T) if use_tqdm else data.T
     if t_vals_cache is None:
-        # use heuristic that mean needs to be greater than popmean to speed up calculation
         return np.array(
-            [stats.ttest_1samp(x[~np.isnan(x)], popmean=popmean, alternative="greater")[0] if x[~np.isnan(
-                x)].mean() > popmean else 0 for x in iterator]
+            [calc_t_value(x, popmean, epsilon) for x in iterator]
         )
     else:
         t_vals = []
@@ -398,6 +412,11 @@ def calc_image_t_values(data, popmean, use_tqdm=False, t_vals_cache=None, precis
                     t_vals.append(t_vals_cache[key])
                 else:
                     t_val = stats.ttest_1samp(x_no_nan, popmean=popmean, alternative="greater")[0]
+                    if np.isinf(t_val):
+                        # Add/subtract epsilon for numerical stability
+                        x_no_nan[0] = x_no_nan + epsilon
+                        x_no_nan[-1] = x_no_nan - epsilon
+                        t_val = stats.ttest_1samp(x_no_nan, popmean=popmean, alternative="greater")[0]
                     t_vals.append(t_val)
                     t_vals_cache[key] = t_val
             else: