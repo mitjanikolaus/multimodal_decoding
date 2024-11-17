fix for numerical stability

analyses/searchlight/searchlight_permutation_testing.py
@@ -386,8 +386,8 @@ def calc_t_value(values, popmean, epsilon=1e-8):
         t_val = stats.ttest_1samp(values_no_nan, popmean=popmean, alternative="greater")[0]
         if np.isinf(t_val):
             # Add/subtract epsilon for numerical stability
-            values_no_nan[0] = values_no_nan + epsilon
-            values_no_nan[-1] = values_no_nan - epsilon
+            values_no_nan[0] = values_no_nan[0] + epsilon
+            values_no_nan[-1] = values_no_nan[-1] - epsilon
             t_val = stats.ttest_1samp(values_no_nan, popmean=popmean, alternative="greater")[0]
             print(f"corrected t val: {t_val}")
         return t_val
@@ -414,8 +414,8 @@ def calc_image_t_values(data, popmean, use_tqdm=False, t_vals_cache=None, precis
                     t_val = stats.ttest_1samp(x_no_nan, popmean=popmean, alternative="greater")[0]
                     if np.isinf(t_val):
                         # Add/subtract epsilon for numerical stability
-                        x_no_nan[0] = x_no_nan + epsilon
-                        x_no_nan[-1] = x_no_nan - epsilon
+                        x_no_nan[0] = x_no_nan[0] + epsilon
+                        x_no_nan[-1] = x_no_nan[-1] - epsilon
                         t_val = stats.ttest_1samp(x_no_nan, popmean=popmean, alternative="greater")[0]
                     t_vals.append(t_val)
                     t_vals_cache[key] = t_val