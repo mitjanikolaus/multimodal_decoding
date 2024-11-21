03cd77a32409eef7e8dc16e47e3a571f0facbd42
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-11-21 17:14:24 +0100

    print cluster p values

--- analyses/searchlight/searchlight_permutation_testing.py
+++ analyses/searchlight/searchlight_permutation_testing.py
@@ -834,7 +834,7 @@ def create_masks(args):
             coords = mesh.coordinates[vertex_max_t_value]
             print(f"Coordinates (max t-value): {coords}")
             clusters_df.append({
-                "hemi": hemi, "id": i, "max t-value": max_t_value, "peak coordinates": np.round(coords, 2)
+                "hemi": hemi, "id": i, "max t-value": max_t_value, "p value": p_values[hemi][vertex_max_t_value], "peak coordinates": np.round(coords, 2)
             })
 
             cluster_map = np.repeat(np.nan, log_10_p_values[hemi].shape)
