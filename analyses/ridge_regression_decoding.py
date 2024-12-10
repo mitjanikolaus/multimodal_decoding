f6d7a748be33076dc335cfef46fc2389fc155fca
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-12-10 11:30:36 +0100

    old z score

--- analyses/ridge_regression_decoding.py
+++ analyses/ridge_regression_decoding.py
@@ -5,7 +5,7 @@ import numpy as np
 import nibabel as nib
 from scipy.spatial.distance import cdist
 from scipy.stats import spearmanr, pearsonr
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, RANSACRegressor
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import GridSearchCV
 import os
@@ -582,17 +582,15 @@ def standardize_fmri_betas(train_fmri_betas, test_fmri_betas, imagery_fmri_betas
                 'std': train_and_test.std(axis=0)}
     pickle.dump(mean_std, open(bold_std_mean_path, 'wb'))
 
-    fmri_betas_transform = load_fmri_betas_transform(subject, training_mode, mask_name)
+    # fmri_betas_transform = load_fmri_betas_transform(subject, training_mode, mask_name)
 
-    # scaler = StandardScaler()
-    # scaler.fit(train_fmri_betas)
-    # train_fmri_betas = scaler.transform(train_fmri_betas)
-    train_fmri_betas = fmri_betas_transform(train_fmri_betas)
+    scaler = StandardScaler()
+    scaler.fit(train_fmri_betas)
+    train_fmri_betas = scaler.transform(train_fmri_betas)
 
     # test_scaler = StandardScaler()
     # test_scaler.fit(test_fmri_betas)
-    # test_fmri_betas = test_scaler.transform(test_fmri_betas)
-    test_fmri_betas = fmri_betas_transform(test_fmri_betas)
+    test_fmri_betas = scaler.transform(test_fmri_betas)
 
     # train_fmri_betas = np.apply_along_axis(func1d=fmri_betas_transform, axis=1, arr=train_fmri_betas)
     #
@@ -600,7 +598,7 @@ def standardize_fmri_betas(train_fmri_betas, test_fmri_betas, imagery_fmri_betas
     # test_fmri_betas_transform = Standardize(test_fmri_betas.mean(axis=0), test_fmri_betas.std(axis=0))
     # test_fmri_betas = np.apply_along_axis(func1d=test_fmri_betas_transform, axis=1, arr=test_fmri_betas)
     if imagery_fmri_betas is not None:
-        imagery_fmri_betas = fmri_betas_transform(imagery_fmri_betas)
+        imagery_fmri_betas = scaler(imagery_fmri_betas)
 
         # imagery_fmri_betas = test_scaler.transform(imagery_fmri_betas)
 
