9204f0be1eb54a2a4469618becfa93fd57dafe21
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-12-11 11:23:47 +0100

    train and test trial betas

--- analyses/ridge_regression_decoding.py
+++ analyses/ridge_regression_decoding.py
@@ -620,7 +620,7 @@ def standardize_fmri_betas(train_fmri_betas, test_fmri_betas, imagery_fmri_betas
     # test_fmri_betas_transform = Standardize(test_fmri_betas.mean(axis=0), test_fmri_betas.std(axis=0))
     # test_fmri_betas = np.apply_along_axis(func1d=test_fmri_betas_transform, axis=1, arr=test_fmri_betas)
     if imagery_fmri_betas is not None:
-        imagery_fmri_betas = test_scaler.transform(imagery_fmri_betas)
+        imagery_fmri_betas = scaler.transform(imagery_fmri_betas)
 
         # imagery_fmri_betas = test_scaler.transform(imagery_fmri_betas)
 
