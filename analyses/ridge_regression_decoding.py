f3050d95cc16386b96abb9df8c44922b00edc833
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2024-12-11 10:27:10 +0100

    new betas

--- analyses/ridge_regression_decoding.py
+++ analyses/ridge_regression_decoding.py
@@ -593,12 +593,13 @@ def standardize_fmri_betas(train_fmri_betas, test_fmri_betas, imagery_fmri_betas
 
     scaler = StandardScaler()
     scaler.fit(train_fmri_betas)
+
     train_fmri_betas = scaler.transform(train_fmri_betas)
     test_fmri_betas = scaler.transform(test_fmri_betas)
 
-    test_fmri_betas = test_fmri_betas * ratio / 2
-    test_stddev = test_fmri_betas.std(axis=0)
-    print(f"test_stddev after: {test_stddev}")
+    # test_fmri_betas = test_fmri_betas * ratio / 2
+    # test_stddev = test_fmri_betas.std(axis=0)
+    # print(f"test_stddev after: {test_stddev}")
 
     # test_scaler = StandardScaler()
 
@@ -609,15 +610,13 @@ def standardize_fmri_betas(train_fmri_betas, test_fmri_betas, imagery_fmri_betas
     # test_scaler.fit(train_and_test)
     # test_fmri_betas = test_scaler.transform(test_fmri_betas)
 
-
-
     # train_fmri_betas = np.apply_along_axis(func1d=fmri_betas_transform, axis=1, arr=train_fmri_betas)
     #
     # test_fmri_betas = np.apply_along_axis(func1d=fmri_betas_transform, axis=1, arr=test_fmri_betas)
     # test_fmri_betas_transform = Standardize(test_fmri_betas.mean(axis=0), test_fmri_betas.std(axis=0))
     # test_fmri_betas = np.apply_along_axis(func1d=test_fmri_betas_transform, axis=1, arr=test_fmri_betas)
-    # if imagery_fmri_betas is not None:
-    #     imagery_fmri_betas = test_scaler.transform(imagery_fmri_betas)
+    if imagery_fmri_betas is not None:
+        imagery_fmri_betas = scaler.transform(imagery_fmri_betas)
 
         # imagery_fmri_betas = test_scaler.transform(imagery_fmri_betas)
 
