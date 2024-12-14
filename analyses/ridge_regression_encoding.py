bed5e9d02fab9523d2ec6c2815f49d3f8f8c4017
mitja <mitja.nikolaus@posteo.de>
2024-12-14 11:56:15 +0100

    less alphas

--- analyses/ridge_regression_encoding.py
+++ analyses/ridge_regression_encoding.py
@@ -261,7 +261,7 @@ def get_args():
     parser.add_argument("--subjects", type=str, nargs='+', default=SUBJECTS)
 
     parser.add_argument("--l2-regularization-alphas", type=float, nargs='+',
-                        default=[1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7])
+                        default=[1e2, 1e3, 1e4, 1e5, 1e6, 1e7])
 
     parser.add_argument("--n-targets-batch", type=int, default=500)
     parser.add_argument("--n-targets-batch-refit", type=int, default=100)
