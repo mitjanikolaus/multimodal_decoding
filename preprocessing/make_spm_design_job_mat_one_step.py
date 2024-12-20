d63331f229cd2a1f08d8946f02140e186287d168
mitja <mitja.nikolaus@posteo.de>
2024-12-20 18:06:17 +0100

    include subj resp trials

--- preprocessing/make_spm_design_job_mat_one_step.py
+++ preprocessing/make_spm_design_job_mat_one_step.py
@@ -35,12 +35,12 @@ def get_condition_names(trial):
     elif trial['stim_name'] == 'Img' and trial['imagert'] == 1:
         conditions.append(f"imagery_{trial['imagery_scene']}")
     else:
-        if (trial['one_back'] != 0) or (trial['subj_resp'] != 0):
-            if trial['one_back'] != 0:
-                conditions.append('one_back')
-            if trial['subj_resp'] != 0:
-                conditions.append('subj_resp')
-        if trial['condition_name'] != 0:
+        # if (trial['one_back'] != 0) or (trial['subj_resp'] != 0):
+        if trial['subj_resp'] != 0:
+            conditions.append('subj_resp')
+        if trial['one_back'] != 0:
+            conditions.append('one_back')
+        elif trial['condition_name'] != 0:
             if trial['trial_type'] == 1 and trial['train_test'] == 1:
                 conditions.append(f"train_image_{trial['condition_name']}")
             if trial['trial_type'] == 2 and trial['train_test'] == 1:
