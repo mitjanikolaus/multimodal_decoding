be4c4496fdc4d9fca018979418a924979d6542a7
mitja <mitja.nikolaus@posteo.de>
2024-12-21 11:25:49 +0100

    ignore subj_resp

--- preprocessing/make_spm_design_job_mat_one_step.py
+++ preprocessing/make_spm_design_job_mat_one_step.py
@@ -34,21 +34,20 @@ def get_condition_names(trial):
         conditions.append('imginst')
     elif trial['stim_name'] == 'Img' and trial['imagert'] == 1:
         conditions.append(f"imagery_{trial['imagery_scene']}")
-    else: #TODO elif
-        # if (trial['one_back'] != 0) or (trial['subj_resp'] != 0):
-        if trial['subj_resp'] != 0:
-            conditions.append('subj_resp')
-        elif trial['one_back'] != 0:
-            conditions.append('one_back')
-        elif trial['condition_name'] != 0:
-            if trial['trial_type'] == 1 and trial['train_test'] == 1:
-                conditions.append(f"train_image_{trial['condition_name']}")
-            if trial['trial_type'] == 2 and trial['train_test'] == 1:
-                conditions.append(f"train_caption_{trial['condition_name']}")
-            if trial['trial_type'] == 1 and trial['train_test'] == 2:
-                conditions.append(f"test_image_{trial['condition_name']}")
-            if trial['trial_type'] == 2 and trial['train_test'] == 2:
-                conditions.append(f"test_caption_{trial['condition_name']}")
+    # if (trial['one_back'] != 0) or (trial['subj_resp'] != 0):
+    # elif trial['subj_resp'] != 0:
+    #     conditions.append('subj_resp')
+    elif trial['one_back'] != 0:
+        conditions.append('one_back')
+    elif trial['condition_name'] != 0:
+        if trial['trial_type'] == 1 and trial['train_test'] == 1:
+            conditions.append(f"train_image_{trial['condition_name']}")
+        if trial['trial_type'] == 2 and trial['train_test'] == 1:
+            conditions.append(f"train_caption_{trial['condition_name']}")
+        if trial['trial_type'] == 1 and trial['train_test'] == 2:
+            conditions.append(f"test_image_{trial['condition_name']}")
+        if trial['trial_type'] == 2 and trial['train_test'] == 2:
+            conditions.append(f"test_caption_{trial['condition_name']}")
 
     if len(conditions) == 0:
         print(f'Unknown condition for trial: {trial}')
