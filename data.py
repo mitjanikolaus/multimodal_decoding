6826cf03835974b424fc0a650e6f0f637f127ec7
mitja <mitja.nikolaus@posteo.de>
2024-12-26 18:12:53 +0100

    separate test scaler

--- data.py
+++ data.py
@@ -603,9 +603,9 @@ def standardize_fmri_betas(train_fmri_betas, test_fmri_betas, imagery_fmri_betas
 
     train_fmri_betas = scaler.transform(train_fmri_betas)
 
-    # test_scaler = StandardScaler()
-    # test_scaler.fit(test_fmri_betas)
-    test_fmri_betas = scaler.transform(test_fmri_betas)
+    test_scaler = StandardScaler()
+    test_scaler.fit(test_fmri_betas)
+    test_fmri_betas = test_scaler.transform(test_fmri_betas)
 
     # print("after standardization:")
     # print(f'train fmri mean: {train_fmri_betas.mean(axis=0).mean():.2f}')
