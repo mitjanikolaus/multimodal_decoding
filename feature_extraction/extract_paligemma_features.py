a66354dab8fdb3c1cfb032a70074d2c6967d4bfe
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2025-02-04 16:23:49 +0100

    debug

--- feature_extraction/extract_paligemma_features.py
+++ feature_extraction/extract_paligemma_features.py
@@ -12,9 +12,8 @@ from data import FUSED_MEAN_FEAT_KEY
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-# device = "cpu"
 
-BATCH_SIZE = 10
+BATCH_SIZE = 20
 
 
 class PaliGemmaFeatureExtractor(FeatureExtractor):
@@ -26,13 +25,8 @@ class PaliGemmaFeatureExtractor(FeatureExtractor):
         inputs = self.preprocessor(
             text=captions, images=images, return_tensors="pt", padding=True,
         )
-        # print(f'input ids : {inputs["input_ids"]}')
-        # print(f'input ids shape: {inputs["input_ids"].shape}')
-        # print(f'pixel_values shape: {inputs["pixel_values"].shape}')
 
         mask = inputs["attention_mask"]
-        # print(f"mask:\n {mask}")
-        # print(f"inputs: {inputs}")
 
         inputs = inputs.to(torch.bfloat16).to(device)
         with torch.no_grad():
@@ -42,19 +36,9 @@ class PaliGemmaFeatureExtractor(FeatureExtractor):
 
         # Average hidden states while ignoring padding tokens
         mask_expanded = mask.unsqueeze(-1).expand((mask.shape[0], mask.shape[1], last_hidden_states.shape[-1]))
-        print(f"last_hidden_states shape {last_hidden_states.shape}")
-        print(f"mask expanded shape {mask_expanded.shape}")
-        print(f"mask_expanded[0]:\n {torch.sum(mask_expanded[0])}")
-        print(f"mask_expanded[1]:\n {torch.sum(mask_expanded[1])}")
-        print(f"mask_expanded[3]:\n {torch.sum(mask_expanded[3])}")
-        print(f"last_hidden_states[0]:\n {last_hidden_states[0]}")
-        print(f"last_hidden_states[1]:\n {last_hidden_states[1]}")
-        print(f"last_hidden_states[3]:\n {last_hidden_states[3]}")
         last_hidden_states[mask_expanded == 0] = 0
 
         feats_fused_mean = last_hidden_states.mean(dim=1)
-        print(f"feats_fused_mean shape {feats_fused_mean.shape}")
-        print(f"feats_fused_mean {feats_fused_mean}")
 
         return {
             FUSED_MEAN_FEAT_KEY: feats_fused_mean,
@@ -62,13 +46,6 @@ class PaliGemmaFeatureExtractor(FeatureExtractor):
 
 
 if __name__ == "__main__":
-    # model_name = "google/paligemma-3b-pt-224"
-    # model = PaliGemmaForConditionalGeneration.from_pretrained(model_name).eval()
-    # processor = PaliGemmaProcessor.from_pretrained(model_name)
-    #
-    # extractor = PaliGemmaFeatureExtractor(model, processor, "paligemma", BATCH_SIZE, device)
-    # extractor.extract_features()
-
     quantization_config = BitsAndBytesConfig(load_in_8bit=True)
 
     model_name = "google/paligemma2-3b-pt-224"
@@ -78,4 +55,13 @@ if __name__ == "__main__":
     processor = PaliGemmaProcessor.from_pretrained(model_name)
 
     extractor = PaliGemmaFeatureExtractor(model, processor, "paligemma2", BATCH_SIZE, device, move_model=False)
+    extractor.extract_features()
+
+    model_name = "google/paligemma-3b-pt-224"
+    model = PaliGemmaForConditionalGeneration.from_pretrained(
+        model_name, torch_dtype=torch.bfloat16, quantization_config=quantization_config, device_map="cuda:0"
+    )
+    processor = PaliGemmaProcessor.from_pretrained(model_name)
+
+    extractor = PaliGemmaFeatureExtractor(model, processor, "paligemma", BATCH_SIZE, move_model=False)
     extractor.extract_features()
