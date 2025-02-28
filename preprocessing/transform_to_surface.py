e25e1c8a0b1e9cd1e99d6715c3fddb8bf0fc4bdb
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2025-02-28 11:45:47 +0100

    transform to surface nan stats

--- preprocessing/transform_to_surface.py
+++ preprocessing/transform_to_surface.py
@@ -16,12 +16,8 @@ from utils import FMRI_BETAS_SURFACE_DIR, SUBJECTS, DEFAULT_RESOLUTION, FMRI_BET
 def run(args):
     for subject in args.subjects:
         print("\n", subject)
-        train_fmri, train_stim_ids, train_stim_types = get_fmri_data_paths(args.betas_dir, subject, SPLIT_TRAIN)
         test_fmri, test_stim_ids, test_stim_types = get_fmri_data_paths(args.betas_dir, subject, SPLIT_TEST)
         imagery_fmri, imagery_stim_ids, imagery_stim_types = get_fmri_data_paths(args.betas_dir, subject, SPLIT_IMAGERY)
-
-        pickle.dump(train_stim_ids, open(os.path.join(FMRI_STIM_INFO_DIR, f"{subject}_stim_ids_train.p"), 'wb'))
-        pickle.dump(train_stim_types, open(os.path.join(FMRI_STIM_INFO_DIR, f"{subject}_stim_types_train.p"), 'wb'))
 
         assert np.all(test_stim_types[INDICES_TEST_STIM_IMAGE] == "image")
         assert np.all(test_stim_types[INDICES_TEST_STIM_CAPTION] == "caption")
@@ -29,7 +25,6 @@ def run(args):
         assert np.all(test_stim_types == TEST_STIM_TYPES)
         assert np.all(imagery_stim_ids == IMAGERY_STIMS_IDS[subject])
         assert np.all(imagery_stim_types == IMAGERY_STIMS_TYPES[subject])
-
         assert np.all(imagery_stim_ids == [i[1] for i in IMAGERY_SCENES[subject]])
 
         gray_matter_mask = get_graymatter_mask_path(subject)
@@ -39,35 +34,59 @@ def run(args):
             print("Hemisphere: ", hemi)
             pial_mesh = fsaverage[f"pial_{hemi}"]
 
+            print("transforming to surface.. (test)", end=" ")
+            surface_projection = surface.vol_to_surf(test_fmri, pial_mesh, mask_img=gray_matter_mask).T
+            print("done.")
+            results_file_name = f"{subject}_{hemi}_{args.resolution}_test.p"
+            pickle.dump(surface_projection, open(os.path.join(args.out_dir, results_file_name), 'wb'))
+            print("saved.")
+            print(f'original nans: {np.sum(np.isnan(test_fmri))} ({np.mean(np.isnan(test_fmri)):.2f}%)')
+            print(f'transformed nans: {np.sum(np.isnan(surface_projection))} ({np.mean(np.isnan(surface_projection)):.2f}%)')
+
+            print("transforming to surface.. (imagery)", end=" ")
+            surface_projection = surface.vol_to_surf(imagery_fmri, pial_mesh, mask_img=gray_matter_mask).T
+            print("done.")
+            results_file_name = f"{subject}_{hemi}_{args.resolution}_imagery.p"
+            pickle.dump(surface_projection, open(os.path.join(args.out_dir, results_file_name), 'wb'))
+            print("saved.")
+
+        train_fmri, train_stim_ids, train_stim_types = get_fmri_data_paths(args.betas_dir, subject, SPLIT_TRAIN)
+        pickle.dump(train_stim_ids, open(os.path.join(FMRI_STIM_INFO_DIR, f"{subject}_stim_ids_train.p"), 'wb'))
+        pickle.dump(train_stim_types, open(os.path.join(FMRI_STIM_INFO_DIR, f"{subject}_stim_types_train.p"), 'wb'))
+
+        for hemi in args.hemis:
+            print("Hemisphere: ", hemi)
+            pial_mesh = fsaverage[f"pial_{hemi}"]
+
             print("transforming to surface.. (train part 1)", end=" ")
-            X = surface.vol_to_surf(train_fmri[:2500], pial_mesh, mask_img=gray_matter_mask).T
+            surface_projection = surface.vol_to_surf(train_fmri[:2500], pial_mesh, mask_img=gray_matter_mask).T
             print("done.")
             results_file_name_1 = f"{subject}_{hemi}_train_1.p"
-            pickle.dump(X, open(os.path.join(args.out_dir, results_file_name_1), 'wb'))
+            pickle.dump(surface_projection, open(os.path.join(args.out_dir, results_file_name_1), 'wb'))
             print("saved.")
 
             print("transforming to surface.. (train part 2)", end=" ")
             pial_mesh = fsaverage[f"pial_{hemi}"]
-            X = surface.vol_to_surf(train_fmri[2500:5000], pial_mesh, mask_img=gray_matter_mask).T
+            surface_projection = surface.vol_to_surf(train_fmri[2500:5000], pial_mesh, mask_img=gray_matter_mask).T
             print("done.")
             results_file_name_2 = f"{subject}_{hemi}_train_2.p"
-            pickle.dump(X, open(os.path.join(args.out_dir, results_file_name_2), 'wb'))
+            pickle.dump(surface_projection, open(os.path.join(args.out_dir, results_file_name_2), 'wb'))
             print("saved.")
 
             print("transforming to surface.. (train part 3)", end=" ")
             pial_mesh = fsaverage[f"pial_{hemi}"]
-            X = surface.vol_to_surf(train_fmri[5000:7500], pial_mesh, mask_img=gray_matter_mask).T
+            surface_projection = surface.vol_to_surf(train_fmri[5000:7500], pial_mesh, mask_img=gray_matter_mask).T
             print("done.")
             results_file_name_3 = f"{subject}_{hemi}_train_3.p"
-            pickle.dump(X, open(os.path.join(args.out_dir, results_file_name_3), 'wb'))
+            pickle.dump(surface_projection, open(os.path.join(args.out_dir, results_file_name_3), 'wb'))
             print("saved.")
 
             print("transforming to surface.. (train part 4)", end=" ")
             pial_mesh = fsaverage[f"pial_{hemi}"]
-            X = surface.vol_to_surf(train_fmri[7500:], pial_mesh, mask_img=gray_matter_mask).T
+            surface_projection = surface.vol_to_surf(train_fmri[7500:], pial_mesh, mask_img=gray_matter_mask).T
             print("done.")
             results_file_name_4 = f"{subject}_{hemi}_train_4.p"
-            pickle.dump(X, open(os.path.join(args.out_dir, results_file_name_4), 'wb'))
+            pickle.dump(surface_projection, open(os.path.join(args.out_dir, results_file_name_4), 'wb'))
             print("saved.")
 
             X_1 = pickle.load(open(os.path.join(args.out_dir, results_file_name_1), 'rb'))
@@ -80,20 +99,6 @@ def run(args):
             os.remove(os.path.join(args.out_dir, results_file_name_2))
             os.remove(os.path.join(args.out_dir, results_file_name_3))
             os.remove(os.path.join(args.out_dir, results_file_name_4))
-
-            print("transforming to surface.. (test)", end=" ")
-            X = surface.vol_to_surf(test_fmri, pial_mesh, mask_img=gray_matter_mask).T
-            print("done.")
-            results_file_name = f"{subject}_{hemi}_{args.resolution}_test.p"
-            pickle.dump(X, open(os.path.join(args.out_dir, results_file_name), 'wb'))
-            print("saved.")
-
-            print("transforming to surface.. (imagery)", end=" ")
-            X = surface.vol_to_surf(imagery_fmri, pial_mesh, mask_img=gray_matter_mask).T
-            print("done.")
-            results_file_name = f"{subject}_{hemi}_{args.resolution}_imagery.p"
-            pickle.dump(X, open(os.path.join(args.out_dir, results_file_name), 'wb'))
-            print("saved.")
 
 
 def get_args():
