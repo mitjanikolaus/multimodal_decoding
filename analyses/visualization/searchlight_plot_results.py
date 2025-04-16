4a6f52cd9d68fd3ca7263138f1b9c9e2eb495e0c
mitja <mitja.nikolaus@posteo.de>
2025-04-16 15:07:28 +0200

    debug

--- analyses/visualization/searchlight_plot_results.py
+++ analyses/visualization/searchlight_plot_results.py
@@ -146,6 +146,8 @@ def plot(args):
             cbar_max = 1000#np.nanmax(np.concatenate((result_values['left'], result_values['right'])))
 
             for hemi in HEMIS:
+                print(result_values[hemi].shape)
+                print(significance_cutoffs[hemi].shape)
                 print(f"frac values above thresh: {np.mean(result_values[hemi] > significance_cutoffs[hemi])}")
                 result_values[result_values[hemi] < significance_cutoffs[hemi]] = 0
             threshold = 1
