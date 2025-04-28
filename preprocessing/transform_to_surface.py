ef4938bd8cadfac3261295b7523dd565e2384bff
mitja <mitja.nikolaus@posteo.de>
2025-04-28 12:11:05 +0200

    refactor transform to surf

--- preprocessing/transform_to_surface.py
+++ preprocessing/transform_to_surface.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 from data import get_fmri_data_paths, INDICES_TEST_STIM_IMAGE, TEST_STIM_IDS, INDICES_TEST_STIM_CAPTION, IMAGERY_SCENES, \
     SPLIT_IMAGERY, SPLIT_TRAIN, SPLIT_TEST, TEST_STIM_TYPES, IMAGERY_STIMS_IDS, IMAGERY_STIMS_TYPES, \
     IMAGE, CAPTION
-from utils import SUBJECTS, FMRI_BETAS_DIR, FS_HEMI_NAMES, FREESURFER_SUBJECTS_DIR
+from utils import SUBJECTS, FMRI_BETAS_DIR, FS_HEMI_NAMES, FREESURFER_SUBJECTS_DIR, HEMIS
 import nibabel as nib
 
 
@@ -33,39 +33,42 @@ def run(args):
 
         train_fmri, _, _ = get_fmri_data_paths(args.betas_dir, subject, SPLIT_TRAIN)
 
-        jobs = []
-        for hemi in args.hemis:
-            for path in tqdm(train_fmri + test_fmri + imagery_fmri):
-                # transform NaNs to zeros
-                img = nib.load(path)
-                img_data = img.get_fdata()
-                img_data[np.isnan(img_data)] = 0
-                img_zeroed = nib.Nifti1Image(img_data, img.affine, img.header)
-                path_img_zeroed = path.replace(args.betas_dir, os.path.join(args.betas_dir, 'nan_to_zero'+os.sep))
-                assert path != path_img_zeroed
-                os.makedirs(os.path.dirname(path_img_zeroed), exist_ok=True)
-                nib.save(img_zeroed, path_img_zeroed)
+        paths = []
+        for path in tqdm(train_fmri + test_fmri + imagery_fmri):
+            paths.append(path)
 
-                path_out = path.replace(args.betas_dir, os.path.join(args.betas_dir, 'surface', hemi+os.sep))
+        def transform(path, betas_dir, overwrite_reg, silent=True):
+            # fist transform NaNs to zeros
+            img = nib.load(path)
+            img_data = img.get_fdata()
+            img_data[np.isnan(img_data)] = 0
+            img_zeroed = nib.Nifti1Image(img_data, img.affine, img.header)
+            path_img_zeroed = path.replace(betas_dir, os.path.join(betas_dir, 'nan_to_zero' + os.sep))
+            assert path != path_img_zeroed
+            os.makedirs(os.path.dirname(path_img_zeroed), exist_ok=True)
+            nib.save(img_zeroed, path_img_zeroed)
+
+            # then transform to surface using freesurfer
+            for hemi in HEMIS:
+                path_out = path.replace(betas_dir, os.path.join(betas_dir, 'surface', hemi + os.sep))
                 path_out = path_out.replace('.nii', '.gii')
                 assert path != path_out
-
                 os.makedirs(os.path.dirname(path_out), exist_ok=True)
-                reg = f"--regheader {subject}" if args.overwrite_reg is None else f"--reg {args.overwrite_reg}"
-                cmd = (f"mri_vol2surf --mov {path_img_zeroed} --o {path_out} --hemi {FS_HEMI_NAMES[hemi]} --trgsubject fsaverage "
-                       f"{reg} --interp trilinear --projfrac-avg 0 1 0.2")  #--projfrac 0.5") # --interp nearest"
-                jobs.append(cmd)
 
-        def exec_command(cmd, silent=True):
-            if silent:
-                cmd = cmd + " >/dev/null 2>&1"
-            result_code = os.system(cmd)
-            if result_code != 0:
-                raise RuntimeError(f"failed to convert {path} to surface {result_code}")
+                reg = f"--regheader {subject}" if overwrite_reg is None else f"--reg {overwrite_reg}"
+                cmd = (
+                    f"mri_vol2surf --mov {path_img_zeroed} --o {path_out} --hemi {FS_HEMI_NAMES[hemi]} --trgsubject fsaverage "
+                    f"{reg} --interp trilinear --projfrac-avg 0 1 0.2")  # --projfrac 0.5") # --interp nearest"
+
+                if silent:
+                    cmd = cmd + " >/dev/null 2>&1"
+                result_code = os.system(cmd)
+                if result_code != 0:
+                    raise RuntimeError(f"failed to convert {path} to surface {result_code}")
 
         Parallel(n_jobs=int(args.n_jobs))(
-            delayed(exec_command)(cmd)
-            for cmd in tqdm(jobs)
+            delayed(transform)(path, args.betas_dir, args.overwrite_reg)
+            for path in tqdm(paths)
         )
 
         shutil.rmtree(os.path.join(args.betas_dir, 'nan_to_zero', subject))
@@ -77,7 +80,6 @@ def get_args():
     parser.add_argument("--betas-dir", type=str, default=FMRI_BETAS_DIR)
 
     parser.add_argument("--subjects", type=str, nargs='+', default=SUBJECTS)
-    parser.add_argument("--hemis", type=str, nargs="+", default=["left", "right"])
 
     parser.add_argument("--overwrite-reg", type=str)
 
