1edbfad8f8ed5d95b90bbfe689848ef8d33a2af5
mitja <mitja.nikolaus@posteo.de>
2025-05-20 16:09:56 +0200

    fix beta file extraction

--- preprocessing/make_spm_design_job_mat_additional_test.py
+++ preprocessing/make_spm_design_job_mat_additional_test.py
@@ -89,7 +89,7 @@ def preprocess_additional_test_event_files(event_files):
 def event_file_path_additional_test(raw_fmri_data_dir, session, subject, task_name, run):
     task_name = 'coco_singletask_imagery'
     if len (run) < 6:
-        run = f'0{run}'
+        run = f"run-0{run.replace('run-','')}"
     return os.path.join(
         raw_fmri_data_dir, "Extra", "events",
         f"{subject}_{session}_task-{task_name}_{run}_events.tsv"
