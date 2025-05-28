7768f49842fea0fa051a07976429d5efd21f95dd
mitja <mitja.nikolaus@posteo.de>
2025-05-28 11:41:52 +0200

    update package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='multimodal_decoding',
+    name='multimodal_decoding_additional_test',
     version='0.1',
     packages=find_packages(),
     url='',
