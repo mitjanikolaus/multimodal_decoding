962b7542866f611ccb93db92b435893777f21a87
mitja <mitja.nikolaus@posteo.de>
2025-06-18 14:49:00 +0200

    update searchlight results maps

--- analyses/decoding/searchlight/searchlight_results_maps.py
+++ analyses/decoding/searchlight/searchlight_results_maps.py
@@ -98,7 +98,7 @@ def create_gifti_results_maps(args):
                         (scores.subject == subj) & (scores.hemi == hemi) & (scores.metric == metric) & (
                                 scores.training_mode == training_mode)
                         ]
-                    path_out = os.path.join(results_dir, subj, f"{training_mode}_decoder{metric}_{FS_HEMI_NAMES[hemi]}.gii")
+                    path_out = os.path.join(results_dir, subj, f"{training_mode}_decoder_{metric}_{FS_HEMI_NAMES[hemi]}.gii")
                     os.makedirs(os.path.dirname(path_out), exist_ok=True)
                     print(f'saving {path_out} ({len(score_hemi_metric)} vertices)')
                     export_to_gifti(score_hemi_metric.value.values, path_out)
