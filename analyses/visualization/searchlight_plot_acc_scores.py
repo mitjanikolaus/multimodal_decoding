e4d27ccde64388edc57f60566a0248e9396326a3
mitja <mitja.nikolaus@posteo.de>
2025-07-08 10:56:50 +0200

    plot acc scores

--- analyses/visualization/searchlight_plot_acc_scores.py
+++ analyses/visualization/searchlight_plot_acc_scores.py
@@ -206,11 +206,11 @@ def run(args):
     results_dir = os.path.join(permutation_results_dir(args), "results")
     os.makedirs(results_dir, exist_ok=True)
 
-    # scores = load_per_subject_scores(args)
-    # scores = add_diff_metrics(scores)
+    scores = load_per_subject_scores(args)
+    scores = add_diff_metrics(scores)
 
     for training_mode in [MODALITY_AGNOSTIC, MODALITY_SPECIFIC_IMAGES, MODALITY_SPECIFIC_CAPTIONS]:
-        # plot_acc_scores(scores, args, results_dir, training_mode=training_mode)
+        plot_acc_scores(scores, args, results_dir, training_mode=training_mode)
 
         create_composite_image(args, results_dir, metrics=[SPLIT_TEST_IMAGES_ATTENDED, SPLIT_TEST_IMAGES_UNATTENDED,
                                                            SPLIT_TEST_CAPTIONS_ATTENDED,
