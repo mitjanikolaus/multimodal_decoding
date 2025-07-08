1eed5b6461ac4cccd1a7859e40c258b0e6c5c2bb
mitja <mitja.nikolaus@posteo.de>
2025-07-08 10:22:36 +0200

    plot acc scores

--- analyses/visualization/searchlight_plot_acc_scores.py
+++ analyses/visualization/searchlight_plot_acc_scores.py
@@ -145,7 +145,7 @@ def create_composite_image(args, results_path, metrics=TEST_SPLITS, training_mod
             img_hemi = append_images(images=imgs_hemis, padding=10, horizontally=False if view == 'ventral' else True)
             imgs_views.append(img_hemi)
 
-        fig = Figure(facecolor="none", figsize=(10, 6))
+        fig = Figure(facecolor="none", figsize=(13, 6))
         fig.text(0, 0.9, metric, fontsize=50, fontweight='bold')
         fig.savefig(results_path + 'tmptitle.png')
         title_img = Image.open(results_path + 'tmptitle.png')
@@ -206,11 +206,11 @@ def run(args):
     results_dir = os.path.join(permutation_results_dir(args), "results")
     os.makedirs(results_dir, exist_ok=True)
 
-    scores = load_per_subject_scores(args)
-    scores = add_diff_metrics(scores)
+    # scores = load_per_subject_scores(args)
+    # scores = add_diff_metrics(scores)
 
     for training_mode in [MODALITY_AGNOSTIC, MODALITY_SPECIFIC_IMAGES, MODALITY_SPECIFIC_CAPTIONS]:
-        plot_acc_scores(scores, args, results_dir, training_mode=training_mode)
+        # plot_acc_scores(scores, args, results_dir, training_mode=training_mode)
 
         create_composite_image(args, results_dir, metrics=[SPLIT_TEST_IMAGES_ATTENDED, SPLIT_TEST_IMAGES_UNATTENDED,
                                                            SPLIT_TEST_CAPTIONS_ATTENDED,
