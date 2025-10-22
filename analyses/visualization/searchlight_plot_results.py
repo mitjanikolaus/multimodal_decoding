ca35a791bdaf76cb5ac367aa38d375f5e0808458
mitja <mitja.nikolaus@posteo.de>
2025-10-22 16:35:24 +0200

    plot results

--- analyses/visualization/searchlight_plot_results.py
+++ analyses/visualization/searchlight_plot_results.py
@@ -345,6 +345,6 @@ def get_args():
 if __name__ == "__main__":
     args = get_args()
 
-    # plot(args)
-    # create_composite_images_of_all_views(args)
+    plot(args)
+    create_composite_images_of_all_views(args)
     create_composite_images_of_metrics(args)
