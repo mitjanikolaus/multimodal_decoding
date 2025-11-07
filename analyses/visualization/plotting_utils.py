a59b47ade91622ab24649f3bccbde5c38a72b807
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2025-11-07 14:28:46 +0100

    results plots

--- analyses/visualization/plotting_utils.py
+++ analyses/visualization/plotting_utils.py
@@ -163,7 +163,7 @@ def _plot_surf_matplotlib_custom(coords, faces, surf_map=None, bg_map=None, bg_o
                     ticks = [int(threshold), round(np.mean([int(threshold), np.ceil(cbar_vmax)]), 1),
                              round(cbar_vmax, 1) - 0.1]
                     cbar_vmin = ticks[0]
-                    cbar_vmax = np.ceil(cbar_vmax)
+                    # cbar_vmax = np.ceil(cbar_vmax)
                     label = f"TFCE"
                 elif metric.split('$')[0] == DIFF:
                     _, training_mode, metric_1, metric_2 = metric.split('$')
