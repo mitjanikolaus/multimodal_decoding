ccbf4efc9ecbd0a476bebf0c0ef22b7b10b6b38f
mitja <mitja.nikolaus@posteo.de>
2025-11-14 12:48:18 +0100

    plot results

--- analyses/visualization/searchlight_plot_results.py
+++ analyses/visualization/searchlight_plot_results.py
@@ -129,7 +129,7 @@ def plot(args):
                 # null_distribution_tfce_values = pickle.load(open(null_distribution_tfce_values_file, 'rb'))
                 # significance_cutoff, _ = calc_significance_cutoff(null_distribution_tfce_values, ref_metric,
                 #                                                   args.p_value_threshold)
-                significance_cutoff = int(np.nanmin(np.concatenate((result_values['left'], result_values['right'])))) #TODO
+                significance_cutoff = 0#int(np.nanmin(np.concatenate((result_values['left'], result_values['right'])))) #TODO
 
             else:
                 null_distribution_tfce_values_file = os.path.join(
