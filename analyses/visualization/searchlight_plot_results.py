47377f00f648bc039f5d53b21cefc668438d0171
mitja <mitja.nikolaus@posteo.de>
2025-11-14 14:50:03 +0100

    plot results

--- analyses/visualization/searchlight_plot_results.py
+++ analyses/visualization/searchlight_plot_results.py
@@ -265,7 +265,7 @@ def plot(args):
                     hemi_label = 'L' if hemi == 'left' else 'R'
                     if view in ['medial', 'lateral']:
                         y_pos = 0.7
-                        x_pos = 0.5 if hemi == 'left' else 0.9
+                        x_pos = 0.1 if hemi == 'left' else 0.9
                     else:
                         y_pos = 0.6 if hemi == 'left' else 0.4
                         x_pos = 0.2
