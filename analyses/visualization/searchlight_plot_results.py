1bbc0ab8f8d385de0e4b37534a0063322c728d63
mitja <mitja.nikolaus@posteo.de>
2025-11-14 14:40:15 +0100

    plot results

--- analyses/visualization/searchlight_plot_results.py
+++ analyses/visualization/searchlight_plot_results.py
@@ -263,7 +263,7 @@ def plot(args):
                         cmap=CMAP_POS_ONLY,
                     )
                     hemi_label = 'L' if hemi == 'left' else 'R'
-                    fig.text(0, 0.2, hemi_label, fontsize=20)
+                    fig.text(0, 0.8, hemi_label, fontsize=20)
                     title = f"{view}_{hemi}"
                     path = os.path.join(atlas_tmp_results_dir, f"{title}.png")
                     plt.savefig(path)
