bfefab9122671a60d5c6a362e3ffac57fa87a698
mitja <mitja.nikolaus@posteo.de>
2026-01-19 13:05:31 +0100

    plot results

--- analyses/visualization/searchlight_plot_acc_scores.py
+++ analyses/visualization/searchlight_plot_acc_scores.py
@@ -216,16 +216,19 @@ def run(args):
     scores = load_per_subject_scores(args)
     # scores = add_diff_metrics(scores)
 
+    make_per_subject_plots = False
     for training_mode in [MODALITY_SPECIFIC_IMAGES, MODALITY_SPECIFIC_CAPTIONS, MODALITY_AGNOSTIC]:
-        plot_acc_scores(scores, args, results_dir, training_mode=training_mode)
+        plot_acc_scores(scores, args, results_dir, training_mode=training_mode,
+                        make_per_subject_plots=make_per_subject_plots)
 
         create_composite_image(args, results_dir, metrics=[TEST_IMAGES_ATTENDED, TEST_IMAGES_UNATTENDED,
                                                            TEST_CAPTIONS_ATTENDED,
                                                            TEST_CAPTIONS_UNATTENDED],# + DIFF_METRICS,
                                file_suffix="_attention_mod", training_mode=training_mode,
-                               make_per_subject_plots=False)
+                               make_per_subject_plots=make_per_subject_plots)
 
-        create_composite_image(args, results_dir, training_mode=training_mode, metrics=[SPLIT_IMAGERY_WEAK])
+        create_composite_image(args, results_dir, training_mode=training_mode, metrics=[SPLIT_IMAGERY_WEAK],
+                               make_per_subject_plots=make_per_subject_plots)
 
 
     # metrics = [TEST_CAPTIONS_ATTENDED, TEST_CAPTIONS_UNATTENDED, 'diff_attended_unattended_captions']
