833571fc7684ba4519895560d6c3d6ef9f3081a9
mitja <mitja.nikolaus@posteo.de>
2026-05-04 11:59:50 +0200

    export t value maps

--- analyses/decoding/searchlight_classification/searchlight_classification_permutation_testing.py
+++ analyses/decoding/searchlight_classification/searchlight_classification_permutation_testing.py
@@ -211,23 +211,29 @@ def create_masks(results_dir, metric, significance_cutoff, tfce_value_threshold,
 
     tfce_values_path = os.path.join(results_dir, f"tfce_values_{metric}.p")
     tfce_values = pickle.load(open(tfce_values_path, "rb"))
+    t_values_path = os.path.join(permutation_results_dir(args), "t_values.p")
+    t_values = pickle.load(open(t_values_path, "rb"))
 
     threshold = significance_cutoff
     if tfce_value_threshold is not None:
         threshold = tfce_value_threshold
         print(f"using tfce value threshold {tfce_value_threshold}")
-    masks = {hemi: copy.deepcopy(tfce_values[hemi]) for hemi in HEMIS}
+    masks_tfce = {hemi: copy.deepcopy(tfce_values[hemi]) for hemi in HEMIS}
     for hemi in HEMIS:
         print(
             f'{hemi} hemi mask size for threshold {threshold:.2f}: {np.mean(tfce_values[hemi] >= threshold):.2f}')
-        masks[hemi][tfce_values[hemi] >= threshold] = 1
-        masks[hemi][tfce_values[hemi] < threshold] = 0
-        masks[hemi][np.isnan(tfce_values[hemi])] = 0
-        masks[hemi] = masks[hemi].astype(np.uint8)
+        masks_tfce[hemi][tfce_values[hemi] >= threshold] = 1
+        masks_tfce[hemi][tfce_values[hemi] < threshold] = 0
+        masks_tfce[hemi][np.isnan(tfce_values[hemi])] = 0
+        masks_tfce[hemi] = masks_tfce[hemi].astype(np.uint8)
 
         path_out = os.path.join(results_maps_path, f"tfce_values_{metric}_{FS_HEMI_NAMES[hemi]}.gii")
         tfce_values[hemi][tfce_values[hemi] < threshold] = 0
         export_to_gifti(tfce_values[hemi], path_out)
+
+        t_values[hemi][metric][t_values[hemi][metric] < 0] = 0
+        path_out = os.path.join(results_maps_path, f"t_values_{metric}_{FS_HEMI_NAMES[hemi]}.gii")
+        export_to_gifti(t_values[hemi][metric], path_out)
 
     # create_results_cluster_masks(masks, results_dir, metric, resolution, radius, n_neighbors, threshold)
 
