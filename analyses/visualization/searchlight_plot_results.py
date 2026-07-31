c85ab4d65e23a87756ff44fb516db2a140f106b2
mitja <mitja.nikolaus@posteo.de>
2026-07-31 16:58:18 +0200

    plot results diff decoders imagery decoding

--- analyses/visualization/searchlight_plot_results.py
+++ analyses/visualization/searchlight_plot_results.py
@@ -118,7 +118,7 @@ def plot(args):
 
             threshold = None
             cbar_min = -100000#-100000# -significance_cutoff
-            cbar_max = None #100000
+            cbar_max = 100000 #100000
             cmap = "cold_hot"
 
         elif "imagery_weak" in result_metric:
