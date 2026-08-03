23e15d8d297f62dcb2d441d4a6be6a90a68a5ed9
mitja <mitja.nikolaus@posteo.de>
2026-08-03 14:36:19 +0200

    plot results diff decoders imagery decoding

--- analyses/visualization/searchlight_plot_results.py
+++ analyses/visualization/searchlight_plot_results.py
@@ -195,8 +195,14 @@ def plot(args):
 
             cmap = CMAP_POS_ONLY
 
+            ref_args = args
+            if result_metric in [METRIC_MOD_INVARIANT_ATTENDED, METRIC_MOD_INVARIANT_UNATTENDED]:
+                ref_args = args.copy()
+                ref_args.model = "imagebind"
+                ref_args.mod_specific_images_model = "imagebind"
+                ref_args.mod_specific_captions_model = "imagebind"
             null_distribution_tfce_values_file = os.path.join(
-                permutation_results_dir(args),
+                permutation_results_dir(ref_args),
                 f"tfce_values_null_distribution_{result_metric}.p"
             )
             cbar_max = np.nanmax(np.concatenate((result_values['left'], result_values['right'])))
