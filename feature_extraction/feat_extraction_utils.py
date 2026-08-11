7a52f5b24b117cdf06190f484bfe15c158526b18
mitja <mitja.nikolaus@posteo.de>
2026-08-11 17:24:25 +0200

    debug

--- feature_extraction/feat_extraction_utils.py
+++ feature_extraction/feat_extraction_utils.py
@@ -138,7 +138,7 @@ class FeatureExtractor:
     def extract_features(self):
         all_feats = dict()
         for ids, captions, img_paths in tqdm(self.dloader):
-            if isinstance(ids[0], int):
+            if not isinstance(ids[0], str):
                 ids = [id.item() for id in ids]
             for id in ids:
                 all_feats[id] = dict()
